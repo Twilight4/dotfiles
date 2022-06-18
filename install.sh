868735c62ccd0446beedda24204ab85215bdc3e1
Twilight4 <99445042+Twilight4@users.noreply.github.com>
2022-06-19 01:10:24 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,18 +5,27 @@
 ########
 mkdir -p "$HOME/.config/nvim"
 mkdir -p "$HOME/.config/nvim/undo"
-ln -sf "$HOME/dotfiles/nvim/init.vim" "$HOME/.config/nvim/init.vim"
+ln -f "$HOME/.config/nvim/init.vim"
 
-########
+##############
+# Xresources #
+##############
+#mkdir -p "$HOME/.config/x"
+#mkdir -p "$HOME/.config/x/undo"
+rm -rf "$HOME/.config/X11"
+ln -s "$HOME/.config/x" "$HOME/.config"
+
+
+#######
 # zsh #
-########
-mkdir -p "$HOME/.config/zsh"
-ln -sf "$HOME/dotfiles/zsh/.zshenv" "$HOME"
-ln -sf "$HOME/dotfiles/zsh/.zshrc" "$HOME/.config/zsh"
-ln -sf "$HOME/dotfiles/zsh/aliases" "$HOME/.config/zsh/zsh-aliases"
+#######
+#mkdir -p "$HOME/.config/zsh"
+#ln -sf "$HOME/dotfiles/zsh/.zshenv" "$HOME"
+#ln -sf "$HOME/dotfiles/zsh/.zshrc" "$HOME/.config/zsh"
+#ln -sf "$HOME/dotfiles/zsh/aliases" "$HOME/.config/zsh/zsh-aliases"
 
 #########
 # Fonts #
 #########
-mkdir -p "$XDG_DATA_HOME"
-cp -rf "$DOTFILES/fonts" "$XDG_DATA_HOME"
+#mkdir -p "$XDG_DATA_HOME"
+#cp -rf "$DOTFILES/fonts" "$XDG_DATA_HOME"
