ee43614536b1b24ec31653fca34aa106da77d9e1
Twilight4 <99445042+Twilight4@users.noreply.github.com>
2022-06-20 20:09:13 +0200

    Update config.py

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -48,12 +48,12 @@ keys = [
     # Toggle between different layouts as defined below
     Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
     Key([mod], "w", lazy.window.kill(), desc="Kill focused window"),
-    Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
-    Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
+    Key([mod, "shift"], "r", lazy.reload_config(), desc="Reload the config"),
+    Key([mod, "shift"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
     Key([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
     
     #Key([], "Print", lazy.spawn(screenshot), desc="print screen"),
-    #Key([mod, "control"], "l", lazy.lock(), desc="lock"),
+    #Key([mod, "shift"], "l", lazy.lock(), desc="lock"),
     #Key([mod], "x", lazy.spawn(), desc="power menu"),
     #Key([mod], "e", lazy.spawn(), desc="edit configs"),
     #Key([mod], "d", lazy.spawn(), desc="dmenu"),
