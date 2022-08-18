557b264d089780b0d6031f237dc037cba34780e6
Twilight4 <99445042+Twilight4@users.noreply.github.com>
2022-08-18 17:34:15 +0200

    Update install_apps.sh

--- .config/install_apps.sh
+++ .config/install_apps.sh
@@ -33,3 +33,30 @@ lines=$(grep -E "$selection" "$apps_path")
 count=$(echo "$lines" | wc -l)
 packages=$(echo "$lines" | awk -F, {'print $2'})
 echo "$selection" "$lines" "$count" >> "/tmp/packages"
+
+pacman -Syu --noconfirm
+
+rm -f /tmp/aur_queue
+
+dialog --title "Let's go!" --msgbox \
+"The system will now install everything you need.\n\n\
+It will take some time.\n\n " \
+13 60
+
+c=0
+echo "$packages" | while read -r line; do
+c=$(( "$c" + 1 ))
+dialog --title "Arch Linux Installation" --infobox \
+"Downloading and installing program $c out of $count: $line..." \
+8 70
+((pacman --noconfirm --needed -S "$line" > /tmp/arch_install 2>&1) \
+|| echo "$line" >> /tmp/aur_queue) \
+|| echo "$line" >> /tmp/arch_install_failed
+if [ "$line" = "zsh" ]; then
+# Set Zsh as default terminal for our user
+chsh -s "$(which zsh)" "$name"
+fi
+if [ "$line" = "networkmanager" ]; then
+systemctl enable NetworkManager.service
+fi
+done
