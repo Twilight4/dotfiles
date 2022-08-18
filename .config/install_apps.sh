a51911c5394c02ad07875c3f47fd637c42b7a581
Twilight4 <99445042+Twilight4@users.noreply.github.com>
2022-08-18 16:48:01 +0200

    Update install_apps.sh

--- .config/install_apps.sh
+++ .config/install_apps.sh
@@ -2,4 +2,4 @@
 
 apps_path="/tmp/apps.csv"
 curl https://raw.githubusercontent.com/<Twiligh4>\
-/dotfiles/master/apps.csv > $apps_path
+/arch_installer/master/apps.csv > $apps_path                  #apps .csv contains all the applications we want to install on our new system
