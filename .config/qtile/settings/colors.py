cb8f48fd53e87b1c54c2847e0e29b0b1d87b02a9
Twilight4 <99445042+Twilight4@users.noreply.github.com>
2022-08-22 17:25:08 +0200

    Create colors.py

---
+++ .config/qtile/settings/colors.py
@@ -0,0 +1,17 @@
+catppuccin = {
+        'bg': '#1d1d2d',
+        'fg': '#cdd6f4',
+        'blue': '#6791c9',
+        'red': '#df5b61',
+        'black': '#24283b',
+        'cyan': '#89dceb',
+        'green': '#81c19b',
+        'orange': '#f5e0dc',
+        'yellow': '#ecd28b',
+        'grey': '#343637',
+        'deep-grey': '#686868',
+        'light-grey': '#d3d3d3',
+        'pink': '#f5c2e7'
+        }
+
+colors = catppuccin
