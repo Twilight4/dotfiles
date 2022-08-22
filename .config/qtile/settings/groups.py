565264cb6ad0fe2ec92ab0367755d7c098e36925
Twilight4 <99445042+Twilight4@users.noreply.github.com>
2022-08-22 17:23:35 +0200

    Create groups.py

---
+++ .config/qtile/settings/groups.py
@@ -0,0 +1,25 @@
+from libqtile.config import Key, Group
+from libqtile.lazy import lazy
+from settings.keys import mod, keys
+
+# groups = [Group(i) for i in "12345"]
+
+groups = [Group(f"{i+1}", label="") for i in range(5)]
+
+for i in groups:
+    keys.extend(
+            [
+                Key(
+                    [mod],
+                    i.name,
+                    lazy.group[i.name].toscreen(),
+                    desc="Switch to group {}".format(i.name),
+                    ),
+                Key(
+                    [mod, "shift"],
+                    i.name,
+                    lazy.window.togroup(i.name, switch_group=True),
+                    desc="Switch to & move focused window to group {}".format(i.name),
+                    ),
+                ]
+            )
