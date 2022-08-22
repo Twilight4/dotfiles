383dc12ee53a4ccb5dc952064b88a5363dd44a94
Twilight4 <99445042+Twilight4@users.noreply.github.com>
2022-08-22 17:20:25 +0200

    Create key_bindings.py

---
+++ .config/qtile/settings/key_bindings.py
@@ -0,0 +1,43 @@
+from libqtile.config import Drag, Click, Key
+from libqtile.lazy import lazy
+from settings.prefs import terminal, browser, file_manager, screenshot_tool
+
+mod = "mod4"
+
+keys = [
+    Key([mod], "h", lazy.layout.left(), desc="Move focus to left"),
+    Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
+    Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
+    Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
+    Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
+
+    Key([mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"),
+    Key([mod, "shift"], "l", lazy.layout.shuffle_right(), desc="Move window to the right"),
+    Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
+    Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
+    
+    Key([mod, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
+    Key([mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"),
+    Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
+    Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
+    Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
+
+    Key([mod, "shift"], "Return", lazy.spawn(terminal), desc="Launch terminal"),
+    Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
+    Key([mod, "w"], lazy.window.kill(), desc="Kill focused window"),
+    Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
+    Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
+    Key([mod], "d", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
+    
+    #Key([mod, "shift"], "f", lazy.spawn(browser), desc="Launch web browser"),
+    #Key([mod, "shift"], "d", lazy.spawn(file_manager), desc="Launch file manager"),
+    #Key([mod, "shift"], "s", lazy.spawn(screenshot_tool), desc="Launch screenshot tool"),
+    #Key([mod, "shift"], "p", lazy.spawn("rofi -show p -modi p:rofi-power-menu -theme ~/.config/qtile/rofi/themes/gradients/style-1.rasi"), desc="Launch power menu"),
+
+]
+
+mouse = [
+        Drag([mod], "Button1", lazy.window.set_position_floating(), start=lazy.window.get_position()),
+        Drag([mod], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()),
+        Click([mod], "Button2", lazy.window.bring_to_front()),
+        ]
