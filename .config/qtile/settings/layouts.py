75bb28f21803d67f8e640d6841b233fdbc779c28
Twilight4 <99445042+Twilight4@users.noreply.github.com>
2022-08-22 18:10:03 +0200

    Update layouts.py

--- .config/qtile/settings/layouts.py
+++ .config/qtile/settings/layouts.py
@@ -1,11 +1,8 @@
-from libqtile import layout
+from libqtile import layout.MonadTall
 
 layouts = [
-        layout.MonadThreeCol(
+        layout.MonadTall(
             margin = 20,
             border_focus = '#89dceb',
+            border_width=2),
             ),
-        layout.Tile(),
-        layout.Matrix(),
-        layout.Floating(),
-        ]
