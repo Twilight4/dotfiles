50d69a6685b523a536abf62e4f0640a93025e54c
Twilight4 <99445042+Twilight4@users.noreply.github.com>
2022-08-22 20:41:08 +0200

    Update groups.py

--- .config/qtile/settings/groups.py
+++ .config/qtile/settings/groups.py
@@ -1,6 +1,8 @@
-from libqtile.config import Key, Group
-from libqtile.lazy import lazy
 from settings.keys import keys
+from libqtile.config import Key, Group
+from libqtile.config import Group, Match
+from libqtile.lazy import lazy
+from libqtile.dgroups import simple_key_binder
 
 groups = [Group("", layout='monadtall'),
           Group("爵", layout='monadtall'),
@@ -8,6 +10,8 @@ groups = [Group("", layout='monadtall'),
           Group("", layout='monadtall'),
           Group("", layout='monadtall'),]
 
+
+dgroups_key_binder = simple_key_binder("mod4")
 for i in groups:
     keys.extend(
         [
