d042bb304513684005ee794ba570b1876b4e1449
Twilight4 <99445042+Twilight4@users.noreply.github.com>
2022-08-22 18:19:06 +0200

    Update groups.py

--- .config/qtile/settings/groups.py
+++ .config/qtile/settings/groups.py
@@ -2,24 +2,26 @@ from libqtile.config import Key, Group
 from libqtile.lazy import lazy
 from settings.key_bindings import mod, keys
 
-# groups = [Group(i) for i in "12345"]
-
-groups = [Group(f"{i+1}", label="") for i in range(5)]
+groups = [Group("", layout='monadtall'),
+          Group("爵", layout='monadtall'),
+          Group("", layout='monadtall'),
+          Group("", layout='monadtall'),
+          Group("", layout='monadtall'),]
 
 for i in groups:
     keys.extend(
-            [
-                Key(
-                    [mod],
-                    i.name,
-                    lazy.group[i.name].toscreen(),
-                    desc="Switch to group {}".format(i.name),
-                    ),
-                Key(
-                    [mod, "shift"],
-                    i.name,
-                    lazy.window.togroup(i.name, switch_group=True),
-                    desc="Switch to & move focused window to group {}".format(i.name),
-                    ),
-                ]
-            )
+        [
+            Key(
+                [mod],
+                i.name,
+                lazy.group[i.name].toscreen(),
+                desc="Switch to group {}".format(i.name),
+            ),
+            Key(
+                [mod, "shift"],
+                i.name,
+                lazy.window.togroup(i.name, switch_group=True),
+                desc="Switch to & move focused window to group {}".format(i.name),
+            ),
+        ]
+    )
