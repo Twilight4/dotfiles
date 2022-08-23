0cc7b78063ea5c60b2316c6e5920855728b12fce
Twilight4 <99445042+Twilight4@users.noreply.github.com>
2022-08-23 12:26:02 +0200

    Update groups.py

--- .config/qtile/settings/groups.py
+++ .config/qtile/settings/groups.py
@@ -10,29 +10,29 @@ groups = [Group("", layout='monadtall'),
           Group("", layout='monadtall'),
           Group("", layout='monadtall'),]
 
-for i in groups:
-    keys.extend(
-        [
+#for i in groups:
+#    keys.extend(
+#        [
             # mod1 + letter of group = switch to group
-            Key(
-                [mod],
-                i.name,
-                lazy.group[i.name].toscreen(),
-                desc="Switch to group {}".format(i.name),
-            ),
+#            Key(
+#                [mod],
+#                i.name,
+#                lazy.group[i.name].toscreen(),
+#                desc="Switch to group {}".format(i.name),
+#            ),
             # mod1 + shift + letter of group = switch to & move focused window to group
-            Key(
-                [mod, "shift"],
-                i.name,
-                lazy.window.togroup(i.name, switch_group=True),
-                desc="Switch to & move focused window to group {}".format(i.name),
-            ),
+#            Key(
+#                [mod, "shift"],
+#                i.name,
+#                lazy.window.togroup(i.name, switch_group=True),
+#                desc="Switch to & move focused window to group {}".format(i.name),
+#            ),
             # Or, use below if you prefer not to switch to that group.
             # # mod1 + shift + letter of group = move focused window to group
             # Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
             #     desc="move focused window to group {}".format(i.name)),
-        ]
-    )
+#        ]
+#    )
 
 for i, g in enumerate(groups):
   keys.extend([
