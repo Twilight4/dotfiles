66a18645632f1f2c346bad91c8b60de9a06093b2
Twilight4 <99445042+Twilight4@users.noreply.github.com>
2022-08-30 14:45:01 +0200

    Update autostart.sh

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -1,9 +1,24 @@
 #!/usr/bin/env zsh
 
+# VBox
 VBoxClient --clipboard &
 VBoxClient --seamless &
 VBoxClient --checkhostversion &
+
+# Bare Metal
 picom --experimental-backends &
 nitrogen --restore &
 exec --no-startup -id dunst &
 polybar &
+
+#Network
+#nm-applet &
+
+# Bluetooth
+#blueman-applet &
+
+# systray battery icon
+## cbatticon -u 5 &
+
+# systray volume
+#volumeicon &
