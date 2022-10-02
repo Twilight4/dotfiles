79ef218a2913753a1c2af86213173c34423a275f
Twilight4 <99445042+Twilight4@users.noreply.github.com>
2022-10-02 10:06:01 +0200

    Update launcher.sh

--- .config/rofi/launcher/launcher.sh
+++ .config/rofi/launcher/launcher.sh
@@ -11,8 +11,8 @@
 ## style-6     style-7     style-8     style-9     style-10
 ## style-11    style-12    style-13    style-14    style-15
 
-dir="$HOME/.config/rofi/launchers/type-2"
-theme='style-1'
+dir="$HOME/.config/rofi/launchers/"
+theme='style'
 
 ## Run
 rofi \
