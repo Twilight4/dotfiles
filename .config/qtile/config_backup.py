e4d3f1b095ebe092db0fe5696e54a3bf2f537a8f
Twilight4 <99445042+Twilight4@users.noreply.github.com>
2022-10-22 09:33:40 +0200

    Update config_backup.py

--- .config/qtile/config_backup.py
+++ .config/qtile/config_backup.py
@@ -165,6 +165,187 @@ extension_defaults = widget_defaults.copy()
 
 # DISCLAIMER: Commented lines do NOT work (at least on VBox)
 screens = [
+    Screen(
+        top=bar.Bar(
+            [
+                widget.Image(
+                    filename='~/.config/qtile/icons/skull.png',
+                    margin=3,
+                    background=colors['bg'],
+                    scale=True,
+                    ),
+                widget.Sep(
+                    foreground=colors['fg'],
+                    background=colors['bg'],
+                    size_percent=100,
+                    padding=8,
+                    ),
+                widget.GroupBox(
+                    highlight_method='line',
+                    this_current_screen_border=colors['white'],
+                    active=colors['white'],
+                    background=colors['bg'],
+                    foreground=colors['fg'],
+                    highlight_color=colors['bg'],
+                    borderwidth=1,
+                    ),
+                widget.Sep(
+                    foreground=colors['fg'],
+                    background=colors['bg'],
+                    size_percent=100,
+                    padding=8,
+                    ),
+                widget.WindowName(
+                    background=colors['white'],
+                    foreground=colors['fg'],
+                    format = "{name}",
+                    empty_group_string = 'Desktop',
+                    width = 120,
+                    ),
+                widget.Sep(
+                    foreground=colors['fg'],
+                    background=colors['bg'],
+                    size_percent=100,
+                    padding=8,
+                    ),
+                widget.Spacer(),
+                widget.Systray(
+                    background=colors['bg'],
+                    ),
+                widget.CheckUpdates(
+                    update_interval = 1800,
+                    foreground=colors['white'],
+                    background=colors['bg'],
+                    colour_have_updates=colors['dark-yellow'],
+                    colour_no_updates=colors['white'],
+                    display_format='ﮮ Updates: {updates}',
+                    no_update_string=' Fully Updated',
+                    ),
+                widget.Sep(
+                    foreground=colors['fg'],
+                    background=colors['bg'],
+                    size_percent=100,
+                    padding=8,
+                    ),
+                widget.Image(
+                    filename='~/.config/qtile/icons/down-arrow.png',
+                    margin=4,
+                    background=colors['bg'],
+                    scale=True,
+                    ),
+                widget.Net(
+                    background=colors['bg'],
+                    foreground=colors['white'],
+                    update_interval = 3,
+                    format = '{down} ',
+                    padding = 0,
+                    ),
+                widget.Image(
+                    filename='~/.config/qtile/icons/cpu.png',
+                    margin=4,
+                    background=colors['bg'],
+                    scale=True,
+                    ),
+                widget.CPU(
+                    foreground=colors['white'],
+                    background=colors['bg'],
+                    format = '{load_percent}% ',
+                    padding = 0,
+                    ),
+                #widget.ThermalZone(
+                    #foreground=colors['white'],
+                    #background=colors['bg'],
+                    #fgcolor_high=colors['dark-yellow'],
+                    #fgcolor_crit=colors['dark-red'],
+                    #fgcolor_normal=colors['white'],
+                    #high=70,
+                    #crit=90,
+                    #format = ' {temp}°C',
+                    #format_crit = ' {temp}°C',
+                    #),
+                widget.Sep(
+                    foreground=colors['fg'],
+                    background=colors['bg'],
+                    size_percent=100,
+                    padding=8,
+                    ),
+                widget.Image(
+                    filename='~/.config/qtile/icons/ram.png',
+                    margin=2,
+                    background=colors['bg'],
+                    scale=True,
+                    ),
+                widget.Memory(
+                    foreground=colors['white'],
+                    background=colors['bg'],
+                    format='{MemUsed: .0f}{mm}/{MemTotal:.0f}{mm} ',
+                    padding=0,
+                    ),
+                #widget.DF(
+                    #warn_space = 40,
+                    #warn_color=colors['dark-red'],
+                    #format = {'{p} ({uf}{m}|{r:.0f}%)'},
+                    #measure = 'G',
+                    #partition = '/',
+                    #background=colors['bg'],
+                    #),
+                widget.Sep(
+                    foreground=colors['fg'],
+                    background=colors['bg'],
+                    size_percent=100,
+                    padding=8,
+                    ),
+                #widget.Volume(
+                    #theme_path='~/.config/qtile/icons/volume.png',
+                    #update_interval = 0.2,
+                    #mute_command =
+                    #volume_down_command =
+                    #volume_up_command =
+                    #step = 2,
+                    #padding = 0,
+                    #),
+                #widget.Battery(
+                    #foreground=colors['white'],
+                    #background=colors['bg'],
+                    #format = '{char} {percent:2.0%}',
+                    #discharge_char = ' ',
+                    #full_char = ' ',
+                    #charge_char = ' ',
+                    #empty_char = ' ',
+                    #low_foreground=colors['dark-yellow'],
+                    #low_percentage = 0.2,
+                    #),
+                #widget.Wlan(
+                    #foreground=colors['white'],
+                    #background=colors['bg'],
+                    #disconnected_message='睊',
+                    #format='{percent:75% 直}',
+                    #),
+                widget.Sep(
+                    foreground=colors['fg'],
+                    background=colors['bg'],
+                    size_percent=100,
+                    padding=8,
+                    ),
+                widget.Image(
+                    filename='~/.config/qtile/icons/clock.png',
+                    margin=4,
+                    background=colors['bg'],
+                    scale=True,
+                    ),
+                widget.Clock(
+                    format='%I:%M %p  ',
+                    foreground=colors['white'],
+                    background=colors['bg'],
+                    padding = 0,
+                    ),
+                 ],
+             20,
+            border_width=[1, 1, 1, 1],  # Draw top and bottom borders
+            border_color=["#66FFFF", "#66FFFF", "#66FFFF", "#66FFFF"],
+            margin=[3, 10, 0, 10],
+            ),
+         ),
       ]
 
 @hook.subscribe.startup
