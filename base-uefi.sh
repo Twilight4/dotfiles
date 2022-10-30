1d44e3986c75c6070e3d93680be36a939767fee4
Twilight4 <99445042+Twilight4@users.noreply.github.com>
2022-10-30 11:14:22 +0100

    Update base-uefi.sh

--- base-uefi.sh
+++ base-uefi.sh
@@ -2,7 +2,7 @@
 
 ln -sf /usr/share/zoneinfo/Europe/Warsaw /etc/localtime
 hwclock --systohc
-sed -i '178s/.//' /etc/locale.gen
+sed -i '171s/.//' /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 echo "archlinux" >> /etc/hostname
