ee5742480a8ff9fe7c14fb84b91670667db2bb2e
Twilight4 <99445042+Twilight4@users.noreply.github.com>
2022-10-30 11:08:13 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,3 @@
-# THIS IS INSTALLATION FOR ONLY MY PERSONAL DOTFILES AND PACKAGES AFTER MINIMAL BASE-INSTALL OF ARCHINSTALL SCRIPT, ITS NOT THE SAME AS IN THE ARCH-INSTALL REPO
-
 #!/bin/bash
 
 run() {
@@ -149,5 +147,9 @@ echo 'Reboot now and then do a post-Installation process:
 6. add ssh pub key to github
 '
 }
+
+/bin/echo -e "\e[1:32mREBOOTING IN 5..4..3..2..1..\e[0n"
+sleep 5
+sudo reboot
 
 run "$@"
