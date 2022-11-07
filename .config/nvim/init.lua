2f3283f9cd7a7f929f403f7400d22a5598946dfd
Twilight4 <99445042+Twilight4@users.noreply.github.com>
2022-11-07 09:25:17 +0100

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,18 +2,3 @@ require('base')
 require('highlights')
 require('maps')
 require('plugins')
-
--- share yanked text with the system clipboard
-local has = function(x)
-  return vim.fn.has(x) == 1
-end
-
-local is_mac = has "macunix"
-local is_win = has "win32"
-
-if is_mac then
-  require('craftzdog.macos')
-end
-if is_win then
-  require('craftzdog.windows')
-end
