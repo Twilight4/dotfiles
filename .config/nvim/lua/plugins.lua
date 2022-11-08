3d84bff79ed4d40f401d49954205d1824b280e77
Twilight4 <99445042+Twilight4@users.noreply.github.com>
2022-11-08 19:47:33 +0100

    Update plugins.lua

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,11 +1,32 @@
+-- Print if packer is not installed
 local status, packer = pcall(require, "packer")
 if (not status) then
   print("Packer is not installed")
   return
 end
 
-vim.cmd [[packadd packer.nvim]]
+-- Automatically install packer
+local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+if fn.empty(fn.glob(install_path)) > 0 then
+  PACKER_BOOTSTRAP = fn.system {
+    "git",
+    "clone",
+    "--depth",
+    "1",
+    "https://github.com/wbthomason/packer.nvim",
+    install_path,
+  }
+  print "Installing packer close and reopen Neovim..."
+  vim.cmd [[packadd packer.nvim]]
+end
 
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+  return
+end
+
+-- Plugins
 packer.startup(function(use)
   use 'wbthomason/packer.nvim'
   use {
