d4824d6fd18f0e42a2a1fa434cc95d1cfbdbaab0
Twilight4 <99445042+Twilight4@users.noreply.github.com>
2022-11-13 14:14:22 +0100

    Update keymaps.lua

--- .config/nvim/lua/twilight/keymaps.lua
+++ .config/nvim/lua/twilight/keymaps.lua
@@ -22,13 +22,23 @@ keymap.set('n', '<C-a>', 'gg<S-v>G')
 -- Better paste
 keymap.set('v', 'p', '"_dP')
 
--- Add buffer
-keymap.set('n','ba', ':badd ') -- add new buffer, type filename
--- Navigate buffers
-keymap.set('n', '<S-l>', ':bnext<CR>')
-keymap.set('n', '<S-h>', ':bprevious<CR>')
--- Close buffers
-keymap.set('n', '<S-q>', '<cmd>Bdelete!<CR>')
+-- Buffers
+keymap.set('n','<S-h>', ':badd ') -- add new buffer, type filename
+keymap.set('n','<S-l>', ':ls!<CR>') -- list all buffers
+keymap.set('n', '<S-j>', ':bnext<CR>') -- buffer next
+keymap.set('n', '<S-k>', ':bprevious<CR>') -- buffer previous
+keymap.set('n', '<S-q>', '<cmd>bdel!<CR>') -- close active buffer
+
+-- Split window
+keymap.set('n', 'sv', '<C-w>v') -- split window vertically
+keymap.set('n', 'ss', '<C-w>s') -- split window horizontally
+keymap.set('n', 'se', '<C-w>=') -- make split windows equal width & height
+keymap.set('', 'sx', ':close<CR>') -- close current split window
+-- Resize window
+keymap.set('n', 'zk', ':resize -3<CR>')
+keymap.set('n', 'zj', ':resize +3<CR>')
+keymap.set('n', 'zh', ':vertical resize -5<CR>')
+keymap.set('n', 'zl', ':vertical resize +5<CR>')
 
 -- Tabs
 keymap.set('n', 'te', ':tabedit ') -- open new tab and type a name
@@ -37,19 +47,8 @@ keymap.set('n', 'tx', ':tabclose<CR>') -- close current tab
 keymap.set('n', 'tn', ':tabn<CR>') --  go to next tab
 keymap.set('n', 'tp', ':tabp<CR>') --  go to previous tab
 
--- Split window
-keymap.set('n', 'sv', '<C-w>v') -- split window vertically
-keymap.set('n', 'ss', '<C-w>s') -- split window horizontally
-keymap.set('n', 'se', '<C-w>=') -- make split windows equal width & height
-keymap.set('', 'sx', ':close<CR>') -- close current split window
--- Resize window
-keymap.set('n', '<M-,>', '<C-w>-')
-keymap.set('n', '<M-n>', '<C-w><')
-keymap.set('n', '<M-m>', '<C-w>+')
-keymap.set('n', '<M-.>', '<C-w>>')
-
 -- Disable arrow keys
-keymap.set('n',  '<Down>', '<NOP>')
-keymap.set('n',  '<Left>', '<NOP>')
-keymap.set('n',  '<Right>', '<NOP>')
-keymap.set('n',  '<Up>', '<NOP>')
+keymap.set('n', '<Down>', '<NOP>')
+keymap.set('n', '<Left>', '<NOP>')
+keymap.set('n', '<Right>', '<NOP>')
+keymap.set('n', '<Up>', '<NOP>')
