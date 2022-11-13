1cdb7d0dc10966a8ea4b9926da255e798b70ae53
Twilight4 <99445042+Twilight4@users.noreply.github.com>
2022-11-13 21:15:10 +0100

    Update zsh-scripts.zsh

--- .config/zsh/zsh-scripts.zsh
+++ .config/zsh/zsh-scripts.zsh
@@ -1,5 +1,47 @@
 #!/usr/bin/env zsh
 
+# upgrade apropos
+manual=$(apropos -s ${SECTION:-''} ${@:-.} | \
+    grep -v -E '^.+ \(0\)' |\
+    awk '{print $2 "    " $1}' | \
+    sort | \
+    dmenu -i -p "Manual: " | \
+    sed -E 's/^\((.+)\)/\1/')
+
+if [ -z "$MANUAL" ]; then
+  man -T${FORMAT:-pdf} $manual | ${READER:-zathura -}
+fi
+
+# Disk
+hdd() {
+  hdd="$(df -h | awk 'NR==4{print $3, $5}')"
+  echo -e "HDD: $hdd"
+}
+
+# RAM
+mem() {
+  mem=`free | awk '/Mem/ {printf "%dM/%dM\n", $3 / 1024.0, $2 / 1024.0 }'`
+  echo -e "$mem"
+}
+
+## CPU
+cpu() {
+  read cpu a b c previdle rest < /proc/stat
+  prevtotal=$((a+b+c+previdle))
+  sleep 0.5
+  read cpu a b c idle rest < /proc/stat
+  total=$((a+b+c+idle))
+  cpu=$((100*( (total-prevtotal) - (idle-previdle) ) / (total-prevtotal) ))
+  echo -e "CPU: $cpu%"
+}
+
+## Volume
+vol() {
+    vol=`amixer get Master | awk -F'[][]' 'END{ print $4":"$2 }' | sed 's/on://g'`
+    echo -e "VOL: $vol"
+}
+
+
 # Fix the file name
 if [[ -f "$XDG_CONFIG_HOME/zsh/plugins/zsh-completions/zsh-completions.plugin.zsh" ]]
 then
