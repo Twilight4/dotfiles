ded7c660ea7a104f9d1248812674de7f718d95cd
Twilight4 <99445042+Twilight4@users.noreply.github.com>
2022-11-13 16:16:34 +0100

    Update keymaps.lua

--- .config/nvim/lua/twilight/keymaps.lua
+++ .config/nvim/lua/twilight/keymaps.lua
@@ -62,3 +62,10 @@ keymap.set('n', 'sm', ':MaximizerToggle<CR>') -- toggle split window maximizatio
 
 -- nvim-tree
 keymap.set("n", "ze", ":NvimTreeToggle<CR>") -- toggle file explorer
+
+-- telescope
+keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>") -- find files within current working directory, respects .gitignore
+keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>") -- find string in current working directory as you type
+keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>") -- find string under cursor in current working directory
+keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>") -- list open buffers in current neovim instance
+keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>") -- list available help tags
