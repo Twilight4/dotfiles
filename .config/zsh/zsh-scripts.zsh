03472545155646fda9c3fef7e2cb3fce626e47de
Twilight4 <99445042+Twilight4@users.noreply.github.com>
2022-11-17 07:04:53 +0100

    Update zsh-scripts.zsh

--- .config/zsh/zsh-scripts.zsh
+++ .config/zsh/zsh-scripts.zsh
@@ -62,25 +62,6 @@ for km in viopp visual; do
   done
 done
 
-# Display current jobs
-function fg-bg() {
-    if [[ $#BUFFER -eq 0 ]]; then
-        local backgroundProgram="$(jobs | tail -n 1 | awk '{print $4}')"
-        case "$backgroundProgram" in
-            "nc"|"ncat"|"netcat"|"resize-netcat-listener"|"rnc")
-                # Make sure that /dev/tty is given to the stty command by doing </dev/tty
-                terminal-size-clip < /dev/tty
-                stty raw -echo < /dev/tty; fg
-                ;;
-            *)
-                fg
-                ;;
-        esac
-    else
-        zle push-input
-    fi
-}
-
 # Display width / height of an image
 imgsize() {
     local width=$(identify -format "%w" "$1")> /dev/null
