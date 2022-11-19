81af3b538bfa17d1412ba7390cc9dc87e593b143
Twilight4 <99445042+Twilight4@users.noreply.github.com>
2022-11-19 14:01:19 +0100

    Update scripts.zsh

--- .config/zsh/scripts.zsh
+++ .config/zsh/scripts.zsh
@@ -341,40 +341,6 @@ touchproject(){
     cd -
 }
 
-# Display... the matrix! - matrix
-matrix () {
-    local lines=$(tput lines)
-    cols=$(tput cols)
-
-    awkscript='
-    {
-        letters="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@#$%^&*()"
-        lines=$1
-        random_col=$3
-        c=$4
-        letter=substr(letters,c,1)
-        cols[random_col]=0;
-        for (col in cols) {
-            line=cols[col];
-            cols[col]=cols[col]+1;
-            printf "\033[%s;%sH\033[2;32m%s", line, col, letter;
-            printf "\033[%s;%sH\033[1;37m%s\033[0;0H", cols[col], col, letter;
-            if (cols[col] >= lines) {
-                cols[col]=0;
-            }
-    }
-}
-'
-
-echo -e "\e[1;40m"
-clear
-
-while :; do
-    echo $lines $cols $(( $RANDOM % $cols)) $(( $RANDOM % 72 ))
-    sleep 0.05
-done | awk "$awkscript"
-}
-
 # Source bash scripts (can't source from .zshrc)
 pipes() {
     "$ZDOTDIR/bash-scripts/pipes.sh" "$@"
@@ -390,4 +356,8 @@ githeat() {
 
 bm() {
     "$ZDOTDIR/bash-scripts/bm.sh" "$@"
+}
+
+matrix() {
+    "$ZDOTDIR/bash-scripts/matrix.sh" "$@"
 }
