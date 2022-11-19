2e19e1f1865e11f0ecad0683f24288f62ec8fde8
Twilight4 <99445042+Twilight4@users.noreply.github.com>
2022-11-19 13:06:59 +0100

    Update zsh-functions.zsh

--- .config/zsh/zsh-functions.zsh
+++ .config/zsh/zsh-functions.zsh
@@ -32,22 +32,3 @@ function zsh_add_completion() {
         completion_file="$(basename "${completion_file_path}")"
         if [ "$2" = true ] && compinit "${completion_file:1}"
 }
-
-# Display current jobs
-function fg-bg() {
-    if [[ $#BUFFER -eq 0 ]]; then
-        local backgroundProgram="$(jobs | tail -n 1 | awk '{print $4}')"
-        case "$backgroundProgram" in
-            "nc"|"ncat"|"netcat"|"resize-netcat-listener"|"rnc")
-                # Make sure that /dev/tty is given to the stty command by doing </dev/tty
-                terminal-size-clip < /dev/tty
-                stty raw -echo < /dev/tty; fg
-                ;;
-            *)
-                fg
-                ;;
-        esac
-    else
-        zle push-input
-    fi
-}
