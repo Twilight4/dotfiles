a6a76ff0f55b74556c6e799f3bb4ce243331a325
Twilight4 <99445042+Twilight4@users.noreply.github.com>
2022-11-19 12:44:31 +0100

    Update zsh-functions.zsh

--- .config/zsh/zsh-functions.zsh
+++ .config/zsh/zsh-functions.zsh
@@ -50,9 +50,3 @@ function fg-bg() {
         zle push-input
     fi
 }
-
-function mach_java_mode() {
-    #THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
-    export SDKMAN_DIR="$HOME/.sdkman"
-    [[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
-}
