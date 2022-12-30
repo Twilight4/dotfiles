8b6ec66ead1c932796c7c1b1467f9000bb4d6ee4
Twilight4 <99445042+Twilight4@users.noreply.github.com>
2022-12-30 15:17:44 +0100

    Update quicklinks-hack.sh

--- .config/rofi/applets/bin/quicklinks-hack.sh
+++ .config/rofi/applets/bin/quicklinks-hack.sh
@@ -3,61 +3,55 @@
 ## Author  : Twilight4
 ## Github  : @Twilight4
 #
-## Applets : Screenshot
+## Applets : Quick Links
 
-# Import Current Theme and Icons
+# Import Current Theme
 source "$HOME"/.config/rofi/applets/shared/theme.bash
 theme="$type/$style"
-iDIR="$HOME/.config/hypr/mako/icons"
-
-# Screenshot
-time=$(date +%Y-%m-%d-%H-%M-%S)
-dir="$(xdg-user-dir PICTURES)/screenshots"
-file="screenshot_${time}_${RANDOM}.png"
 
 # Theme Elements
-prompt='Screenshot'
-mesg="DIR: `xdg-user-dir PICTURES`/screenshots"
+prompt='Quick Links'
+mesg="Using '$BROWSER' as web browser"
 
-if [[ "$theme" == *'type-1'* ]]; then
+if [[ ( "$theme" == *'type-1'* ) || ( "$theme" == *'type-3'* ) || ( "$theme" == *'type-5'* ) ]]; then
 	list_col='1'
-	list_row='5'
-	win_width='500px'
-elif [[ "$theme" == *'type-3'* ]]; then
-	list_col='1'
-	list_row='5'
-	win_width='120px'
-elif [[ "$theme" == *'type-5'* ]]; then
-	list_col='1'
-	list_row='5'
-	win_width='520px'
+	list_row='6'
 elif [[ ( "$theme" == *'type-2'* ) || ( "$theme" == *'type-4'* ) ]]; then
-	list_col='5'
+	list_col='6'
 	list_row='1'
-	win_width='670px'
+fi
+
+if [[ ( "$theme" == *'type-1'* ) || ( "$theme" == *'type-5'* ) ]]; then
+	efonts="JetBrains Mono Nerd Font 10"
+else
+	efonts="JetBrains Mono Nerd Font 28"
 fi
 
 # Options
 layout=`cat ${theme} | grep 'USE_ICON' | cut -d'=' -f2`
 if [[ "$layout" == 'NO' ]]; then
-	option_1=" Capture Desktop"
-	option_2=" Capture Area"
-	option_3=" Capture Window"
-	option_4=" Capture in 5s"
-	option_5=" Capture in 10s"
+	option_1=" Hack The Box"
+	option_2=" TryHackMe"
+	option_3=" PortSwigger"
+	option_4=" Proving Grounds"
+	option_5=" PentesterLab"
+	option_6=" Root Me"
+	option_7=" PWNX"
 else
-	option_1=""
-	option_2=""
-	option_3=""
-	option_4=""
-	option_5=""
+	option_1=""
+	option_2=""
+	option_3=""
+	option_4=""
+	option_5=""
+	option_6=""
+	option_7=""
 fi
 
 # Rofi CMD
 rofi_cmd() {
-	rofi -theme-str "window {width: $win_width;}" \
-		-theme-str "listview {columns: $list_col; lines: $list_row;}" \
-		-theme-str 'textbox-prompt-colon {str: "";}' \
+	rofi -theme-str "listview {columns: $list_col; lines: $list_row;}" \
+		-theme-str 'textbox-prompt-colon {str: "";}' \
+		-theme-str "element-text {font: \"$efonts\";}" \
 		-dmenu \
 		-p "$prompt" \
 		-mesg "$mesg" \
@@ -67,77 +61,25 @@ rofi_cmd() {
 
 # Pass variables to rofi dmenu
 run_rofi() {
-	echo -e "$option_1\n$option_2\n$option_3\n$option_4\n$option_5" | rofi_cmd
-}
-
-if [[ ! -d "$dir" ]]; then
-	mkdir -p "$dir"
-fi
-
-# notify and view screenshot
-notify_view() {
-notify_cmd_shot="notify-send -h string:x-canonical-private-synchronous:shot-notify -u low -i ${iDIR}/picture.png"
-	${notify_cmd_shot} "Copied to clipboard."
-	imv ${dir}/"$file"
-	if [[ -e "$dir/$file" ]]; then
-		${notify_cmd_shot} "Screenshot Saved."
-	else
-		${notify_cmd_shot} "Screenshot Deleted."
-	fi
-}
-
-# countdown
-countdown () {
-	for sec in $(seq $1 -1 1); do
-		notify-send -h string:x-canonical-private-synchronous:shot-notify -t 1000 -i "$iDIR"/timer.png "Taking shot in : $sec"
-		sleep 1
-	done
-}
-
-# take shots
-shotnow () {
-	sleep 0.6 && cd ${dir} && grim - | tee "$file" | wl-copy
-	notify_view
-}
-
-shot5 () {
-	countdown '5'
-	sleep 1 && cd ${dir} && grim - | tee "$file" | wl-copy
-	notify_view
-}
-
-shot10 () {
-	countdown '10'
-	sleep 1 && cd ${dir} && grim - | tee "$file" | wl-copy
-	notify_view
-}
-
-shotwin () {
-	w_pos=$(hyprctl activewindow | grep 'at:' | cut -d':' -f2 | tr -d ' ' | tail -n1)
-	w_size=$(hyprctl activewindow | grep 'size:' | cut -d':' -f2 | tr -d ' ' | tail -n1 | sed s/,/x/g)
-	cd ${dir} && grim -g "$w_pos $w_size" - | tee "$file" | wl-copy
-	notify_view
-}
-
-shotarea () {
-	cd ${dir} && grim -g "$(slurp -b 1B1F28CC -c E06B74ff -s C778DD0D -w 2)" - | tee "$file" | wl-copy
-	notify_view
+	echo -e "$option_1\n$option_2\n$option_3\n$option_4\n$option_5\n$option_6" | rofi_cmd
 }
 
 # Execute Command
 run_cmd() {
-	if [[ "$1" == '--now' ]]; then
-		shotnow
-	elif [[ "$1" == '--area' ]]; then
-		shotarea
-	elif [[ "$1" == '--win' ]]; then
-		shotwin
-	elif [[ "$1" == '--in5' ]]; then
-		shot5
-	elif [[ "$1" == '--in10' ]]; then
-		shot10
-else
-	echo -e "Available Options : --now --in5 --in10 --win --area"
+	if [[ "$1" == '--opt1' ]]; then
+		xdg-open 'https://www.hackthebox.com/'
+	elif [[ "$1" == '--opt2' ]]; then
+		xdg-open 'https://tryhackme.com/'
+	elif [[ "$1" == '--opt3' ]]; then
+		xdg-open 'https://portswigger.net/web-security/all-labs'
+	elif [[ "$1" == '--opt4' ]]; then
+		xdg-open 'https://www.offensive-security.com/labs/'
+	elif [[ "$1" == '--opt5' ]]; then
+		xdg-open 'https://pentesterlab.com/'
+	elif [[ "$1" == '--opt6' ]]; then
+		xdg-open 'https://www.root-me.org/?lang=en/'
+	elif [[ "$1" == '--opt7' ]]; then
+		xdg-open 'https://pwnx.io/'
 	fi
 }
 
@@ -145,18 +87,24 @@ else
 chosen="$(run_rofi)"
 case ${chosen} in
     $option_1)
-		run_cmd --now
+		run_cmd --opt1
         ;;
     $option_2)
-		run_cmd --area
+		run_cmd --opt2
         ;;
     $option_3)
-		run_cmd --win
+		run_cmd --opt3
         ;;
     $option_4)
-		run_cmd --in5
+		run_cmd --opt4
         ;;
     $option_5)
-		run_cmd --in10
+		run_cmd --opt5
+        ;;
+    $option_6)
+		run_cmd --opt6
+        ;;
+    $option_7)
+		run_cmd --opt6
         ;;
 esac
