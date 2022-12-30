5089d00d9fde25dfd8903b31641146f646a1756c
Twilight4 <99445042+Twilight4@users.noreply.github.com>
2022-12-30 11:00:41 +0100

    Update aliases.zsh

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -5,11 +5,70 @@ alias l='lsd'
 alias ls='lsd -l'
 alias la='lsd -la'
 alias lt='lsd --tree'
+alias lx='lsd -lXBh'                # sort by extension
+alias lk='lsd -lSrh'                # sort by size
+alias lc='lsd -lcrh'                # sort by change time
+alias lu='lsd -lurh'                # sort by access time
+alias lm='lsd -alh |more'           # pipe through 'more'
+alias lf="lsd -l | egrep -v '^d'"   # files only
+alias ldir="lsd -l | egrep '^d'"    # directories only
+
+# Alias's to modified commands
+alias mkdir='mkdir -p'
+alias ping='ping -c 5'        
+alias less='less -R'
+alias svi='sudo vi'
+alias train='sl | lolcat'
+alias devil='fortune | cowsay -f eyes | lolcat'
+
+# python
+alias pyserver='python3 -m http.server'
+
+#continue download
+alias wget="wget -c"
+
+# alias to show the date
+alias da='date "+%Y-%m-%d %A %T %Z"'
+
+# grub update
+alias update-grub="sudo grub-mkconfig -o /boot/grub/grub.cfg"
+
+#add new fonts
+alias update-fc='sudo fc-cache -fv'
+
+#hardware info --short
+alias hw="hwinfo --short"
+
+#check vulnerabilities microcode
+alias microcode='grep . /sys/devices/system/cpu/vulnerabilities/*'
+
+#Recent Installed Packages
+alias rip="expac --timefmt='%Y-%m-%d %T' '%l\t%n %v' | sort | tail -200 | nl"
+alias riplong="expac --timefmt='%Y-%m-%d %T' '%l\t%n %v' | sort | tail -3000 | nl"
+
+#get the error messages from journalctl
+alias jctl="journalctl -p 3 -xb"
+
+#gpg
+#verify signature for isos
+alias gpg-check="gpg2 --keyserver-options auto-key-retrieve --verify"
+alias fix-gpg-check="gpg2 --keyserver-options auto-key-retrieve --verify"
+#receive the key of a developer
+alias gpg-retrieve="gpg2 --keyserver-options auto-key-retrieve --receive-keys"
+alias fix-gpg-retrieve="gpg2 --keyserver-options auto-key-retrieve --receive-keys"
+alias fix-keyserver="[ -d ~/.gnupg ] || mkdir ~/.gnupg ; cp /etc/pacman.d/gnupg/gpg. conf ~/.gnupg/ ; echo 'done'"
 
 # other
 alias tks='tmux kill-server'                               # tmux
-alias yt3-mp3="yt-dlp --extract-audio --audio-format mp3 " # yt-dlp
 alias wget=wget --hsts-file="$XDG_DATA_HOME/wget-hsts"     # wget
+
+# yt-dlp
+alias yta-aac="yt-dlp --extract-audio --audio-format aac "
+alias yta-best="yt-dlp --extract-audio --audio-format best "
+alias yta-flac="yt-dlp --extract-audio --audio-format flac "
+alias yta-mp3="yt-dlp --extract-audio --audio-format mp3 "
+alias ytv-best="yt-dlp -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+          bestaudio' --merge-output-format mp4 "
+alias yt3-mp3="yt-dlp --extract-audio --audio-format mp3 " 
 
 # git
 alias ga='git add'
@@ -22,12 +81,14 @@ alias pacsyu='sudo pacman -Syu'                  # update only standard pkgs
 alias pacsyyu='sudo pacman -Syyu'                # Refresh pkglist & update standard pkgs
 alias yaysua='yay -Sua --noconfirm'              # update only AUR pkgs (yay)
 alias yaysyu='yay -Syu --noconfirm'              # update standard pkgs and AUR pkgs (yay)
-alias cleanup='yay -Sc'                          # remove orphaned packages
+alias yaysc='yay -Sc'                          # remove orphaned packages
 
 # colorize grep output
 alias grep='grep --color=auto'
 alias egrep='egrep --color=auto'
 alias fgrep='fgrep --color=auto'
+#search content with ripgrep
+alias rg="rg --sort path"
 
 # systemd
 alias gcm="git checkout master"
