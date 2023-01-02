2e5f2e85bb486941345a829dd67606206099bf99
Twilight4 <99445042+Twilight4@users.noreply.github.com>
2023-01-02 00:00:37 +0000

    Update clipboard.sh

--- .config/rofi/applets/bin/clipboard.sh
+++ .config/rofi/applets/bin/clipboard.sh
@@ -2,15 +2,6 @@
 
 theme="launcher/style.rasi"
 dir="$HOME/.config/rofi"
-styles=("$(ls -p --hide="colors.rasi" "$dir"/styles)")
-color="${styles[$(($RANDOM % 10))]}"
-
-# comment this line to disable random colors
-#sed -i -e "s/@import .*/@import \"$color\"/g" $dir/styles/colors.rasi
-
-# comment these lines to disable random style
-#themes=($(ls -p --hide="launcher.sh" --hide="styles" $dir))
-#theme="${themes[$(( $RANDOM % 7 ))]}"
 
 if [[ "$XDG_SESSION_DESKTOP" == "Hyprland" ]]; then
 	# clipman pick --tool=CUSTOM --tool-args="rofi -p \
