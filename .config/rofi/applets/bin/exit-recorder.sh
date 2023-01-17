34499bec8f84ff49472dc4b8886734a23c75173c
Twilight4 <99445042+Twilight4@users.noreply.github.com>
2023-01-17 23:51:08 +0100

    Create exit-recorder.sh

---
+++ .config/rofi/applets/bin/exit-recorder.sh
@@ -0,0 +1,11 @@
+OnExit {
+  [[ -f $TmpRecordPath ]] && rm -f "$TmpRecordPath"
+  	[[ -f $TmpPalettePath ]] && rm -f "$TmpPalettePath"
+    killall --user $USER  --ignore-case  --signal INT  ffmpeg
+    sleep 2
+    pkill -fxn '(/\S+)*ffmpeg\s.*\sx11grab\s.*'
+    pkill -fxn '(/\S+)*ffmpeg\s.*\spulse\s.*'
+    exit 1 
+}
+
+trap OnExit EXIT
