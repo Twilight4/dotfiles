eb67364cc999caa77a9fba30d758fc32282fddfc
Twilight4 <99445042+Twilight4@users.noreply.github.com>
2023-01-21 13:33:50 +0100

    Update functions.zsh

--- .config/zsh/functions.zsh
+++ .config/zsh/functions.zsh
@@ -35,23 +35,6 @@ ex ()
   fi
 }
 
-# Enable shell integrations
-precmd() {
-    print -Pn "\e]133;A\e\\"
-}
-
-# Spawning new terminal instances in the current working directory
-function osc7 {
-    local LC_ALL=C
-    export LC_ALL
-
-    setopt localoptions extendedglob
-    input=( ${(s::)PWD} )
-    uri=${(j::)input/(#b)([^A-Za-z0-9_.\!~*\'\(\)-\/])/%${(l:2::0:)$(([##16]#match))}}
-    print -n "\e]7;file://${HOSTNAME}${uri}\e\\"
-}
-add-zsh-hook -Uz chpwd osc7
-
 # New command
 function _new_command {
     zle push-input
