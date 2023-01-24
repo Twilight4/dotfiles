394037c7fd1f61a5956e9fd329b4f3fcec10b236
Twilight4 <99445042+Twilight4@users.noreply.github.com>
2023-01-24 22:12:02 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,6 @@ run() {
     install-apps
     #enable-blackarch
     #create-directories
-    install-wifi-driver
     install-dotfiles
 }
 
@@ -84,19 +83,6 @@ install-apps() {
 #create-directories() {
 #sudo mkdir -p "/home/$(whoami)/{Document,Download,Video,workspace,Music}"
 #}
-
-install-wifi-driver() {
-    git clone https://github.com/Mange/rtl8192eu-linux-driver
-    cd rtl8192eu-linux-driver
-    sudo dkms add .
-    sudo dkms install rtl8192eu/1.0
-    cd -
-    echo "blacklist rtl8xxxu" | sudo tee /etc/modprobe.d/rtl8xxxu.conf
-    echo -e "8192eu\n\nloop" | sudo tee /etc/modules
-    echo "options 8192eu rtw_power_mgnt=0 rtw_enusbss=0" | sudo tee /etc/modprobe.d/8192eu.conf
-    sudo grub-mkconfig -o /boot/grub/grub.cfg
-    rm -rf rtl8192eu-linux-driver
-}
 
 install-dotfiles() {
     DOTFILES="/tmp/dotfiles"
