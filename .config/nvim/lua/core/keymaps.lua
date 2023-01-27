d7f8291b75552c39175f9408b6ead5f2163d4be9
Twilight4 <99445042+Twilight4@users.noreply.github.com>
2023-01-27 20:46:11 +0100

    Update keymaps.lua

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -72,3 +72,8 @@ keymap.set("n", "ns", "<cmd>Telescope live_grep<cr>") -- find string in current 
 keymap.set("n", "nc", "<cmd>Telescope grep_string<cr>") -- find string under cursor in current working directory
 keymap.set("n", "nb", "<cmd>Telescope buffers<cr>") -- list open buffers in current neovim instance
 keymap.set("n", "nt", "<cmd>Telescope help_tags<cr>") -- list available help tags
+-- telescope git commands
+keymap.set("n", "gc", "<cmd>Telescope git_commits<cr>") -- list all git commits (use <cr> to checkout) ["gc" for git commits]
+keymap.set("n", "gfc", "<cmd>Telescope git_bcommits<cr>") -- list git commits for current file/buffer (use <cr> to checkout) ["gfc" for git file commits]
+keymap.set("n", "gb", "<cmd>Telescope git_branches<cr>") -- list git branches (use <cr> to checkout) ["gb" for git branch]
+keymap.set("n", "gs", "<cmd>Telescope git_status<cr>") -- list current changes per file with diff preview ["gs" for git status]
