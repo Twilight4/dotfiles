dcada347ab47943c6486bdeec3f39fde719b923e
Twilight4 <99445042+Twilight4@users.noreply.github.com>
2023-01-28 05:22:43 +0100

    Update keymaps.lua

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -110,5 +110,4 @@ keymap.set('n', 'zt', ':ToggleTerm size=40 dir=~/Desktop direction=horizontal<CR
 -- Other
 keymap.set('n', '<leader>p', ':PasteImg <CR>')
 keymap.set('n', '<leader>n', ':ASToggle<CR>')
-keymap.set('n', '<leader>m', '::MarkdownPreview<CR>')
-keymap.set('n', '<leader>ms', '::MarkdownPreviewStop<CR>')
+keymap.set('n', '<leader>m', '::MarkdownPreviewToggle<CR>')
