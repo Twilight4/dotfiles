7d932ae918eb4b07bc791cae1f0149529e2f8fa4
Twilight4 <99445042+Twilight4@users.noreply.github.com>
2023-01-28 02:15:36 +0100

    Update keymaps.lua

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -39,10 +39,15 @@ keymap.set('n', 'ss', '<C-w>s') -- split window horizontally
 keymap.set('n', 'sx', ':close<CR>') -- close current split window
 keymap.set('n', 'se', '<C-w>=') -- make split windows equal width & height
 -- Resize window
-keymap.set('n', 'zk', ':resize -3<CR>')
-keymap.set('n', 'zj', ':resize +3<CR>')
-keymap.set('n', 'zh', ':vertical resize -5<CR>')
-keymap.set('n', 'zl', ':vertical resize +5<CR>')
+keymap.set('n', '<C-Up>', ':resize -3<CR>')
+keymap.set('n', '<C-Down>', ':resize +3<CR>')
+keymap.set('n', '<C-Left>', ':vertical resize -5<CR>')
+keymap.set('n', '<C-Right>', ':vertical resize +5<CR>')
+-- Navigate windows with C-h/j/k/l (tmux plugin does it by default)
+--keymap.set('', '<C-h>', '<C-w>h')
+--keymap.set('', '<C-k>', '<C-w>k')
+--keymap.set('', '<C-j>', '<C-w>j')
+--keymap.set('', '<C-l>', '<C-w>l')
 -- Move window
 --keymap.set('', 'sh', '<C-w>h')
 --keymap.set('', 'sk', '<C-w>k')
@@ -56,11 +61,17 @@ keymap.set('n', 'tx', ':tabclose<CR>') -- close current tab
 keymap.set('n', 'tn', ':tabn<CR>') --  go to next tab
 keymap.set('n', 'tp', ':tabp<CR>') --  go to previous tab
 
--- Disable arrow keys
-keymap.set('n', '<Down>', '<NOP>')
-keymap.set('n', '<Left>', '<NOP>')
-keymap.set('n', '<Right>', '<NOP>')
-keymap.set('n', '<Up>', '<NOP>')
+-- Navigate buffers
+keymap('n', '<S-l>', ':bnext<CR>')
+keymap('n', '<S-h>', ':bprevious<CR>')
+-- Close buffers
+keymap('n', '<S-q>', '<cmd>Bdelete!<CR>')
+
+-- Visual --
+-- Stay in indent mode
+keymap('v', '<', '<gv')
+keymap('v', '>', '>gv')
+
 
 ---------------------
 -- Plugin Keybinds --
@@ -73,11 +84,6 @@ keymap.set('n', 'sm', ':MaximizerToggle<CR>') -- toggle split window maximizatio
 keymap.set("n", "ze", ":NvimTreeToggle<CR>") -- toggle file explorer
 
 -- telescope
---keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>") -- find files within current working directory, respects .gitignore
---keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>") -- find string in current working directory as you type
---keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>") -- find string under cursor in current working directory
---keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>") -- list open buffers in current neovim instance
---keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>") -- list available help tags
 keymap.set("n", "nf", "<cmd>Telescope find_files<cr>") -- find files within current working directory, respects .gitignore
 keymap.set("n", "ns", "<cmd>Telescope live_grep<cr>") -- find string in current working directory as you type
 keymap.set("n", "nc", "<cmd>Telescope grep_string<cr>") -- find string under cursor in current working directory
