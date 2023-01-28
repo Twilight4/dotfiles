60808d3eb80f218c01b9c63c574887d3a43e699e
Twilight4 <99445042+Twilight4@users.noreply.github.com>
2023-01-28 23:25:53 +0000

    Add files via upload

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -1,195 +1,194 @@
-;;; config/use-package/init.el -*- lexical-binding: t; -*-
-;;; Commentary:
+;;; init.el -*- lexical-binding: t; -*-
+
+;; This file controls what Doom modules are enabled and what order they load
+;; in. Remember to run 'doom sync' after modifying it!
+
+;; NOTE Press 'SPC h d h' (or 'C-h d h' for non-vim users) to access Doom's
+;;      documentation. There you'll find a link to Doom's Module Index where all
+;;      of our modules are listed, including what flags they support.
+
+;; NOTE Move your cursor over a module's name (or its flags) and press 'K' (or
+;;      'C-c c k' for non-vim users) to view its documentation. This works on
+;;      flags as well (those symbols that start with a plus).
 ;;
-;; In the near future, Doom will stop using `use-package' internally (in its
-;; modules and core), because it's far too complex and magical for our needs.
-;; However, folks who still want to use it may enable this module. However, it
-;; will be enabled by default until Doom no longer depends on it.
-;;
-;;; Code:
+;;      Alternatively, press 'gd' (or 'C-c c d') on a module to browse its
+;;      directory (for easy access to its source code).
 
-(defvar doom--deferred-packages-alist '(t))
-(autoload 'use-package "use-package-core" nil nil t)
+(doom! :input
+       ;;bidi              ; (tfel ot) thgir etirw uoy gnipleh
+       ;;chinese
+       ;;japanese
+       ;;layout            ; auie,ctsrnm is the superior home row
 
+       :completion
+       company           ; the ultimate code completion backend
+       ;;helm              ; the *other* search engine for love and life
+       ;;ido               ; the other *other* search engine...
+       ;;ivy               ; a search engine for love and life
+       vertico           ; the search engine of the future
 
-;;
-;;; Use-package modifications
+       :ui
+       ;;deft              ; notational velocity for Emacs
+       doom              ; what makes DOOM look the way it does
+       doom-dashboard    ; a nifty splash screen for Emacs
+       ;;doom-quit         ; DOOM quit-message prompts when you quit Emacs
+       ;;(emoji +unicode)  ; 🙂
+       hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
+       ;;hydra
+       ;;indent-guides     ; highlighted indent columns
+       ;;ligatures         ; ligatures and symbols to make your code pretty again
+       ;;minimap           ; show a map of the code on the side
+       modeline          ; snazzy, Atom-inspired modeline, plus API
+       ;;nav-flash         ; blink cursor line after big motions
+       ;;neotree           ; a project drawer, like NERDTree for vim
+       ophints           ; highlight the region an operation acts on
+       (popup +defaults)   ; tame sudden yet inevitable temporary windows
+       ;;tabs              ; a tab bar for Emacs
+       ;;treemacs          ; a project drawer, like neotree but cooler
+       ;;unicode           ; extended unicode support for various languages
+       (vc-gutter +pretty) ; vcs diff in the fringe
+       vi-tilde-fringe   ; fringe tildes to mark beyond EOB
+       ;;window-select     ; visually switch windows
+       workspaces        ; tab emulation, persistence & separate workspaces
+       ;;zen               ; distraction-free coding or writing
 
-(setq use-package-compute-statistics init-file-debug
-      use-package-verbose init-file-debug
-      use-package-minimum-reported-time (if init-file-debug 0 0.1)
-      use-package-expand-minimally (not noninteractive))
+       :editor
+       (evil +everywhere); come to the dark side, we have cookies
+       file-templates    ; auto-snippets for empty files
+       fold              ; (nigh) universal code folding
+       ;;(format +onsave)  ; automated prettiness
+       ;;god               ; run Emacs commands without modifier keys
+       ;;lispy             ; vim for lisp, for people who don't like vim
+       ;;multiple-cursors  ; editing in many places at once
+       ;;objed             ; text object editing for the innocent
+       ;;parinfer          ; turn lisp into python, sort of
+       ;;rotate-text       ; cycle region at point between text candidates
+       snippets          ; my elves. They type so I don't have to
+       ;;word-wrap         ; soft wrapping with language-aware indent
 
-;; A common mistake for new users is that they inadvertently install their
-;; packages with package.el, by copying over old `use-package' declarations with
-;; an :ensure t property. Doom doesn't use package.el, so this will throw an
-;; error that will confuse beginners, so we disable `:ensure'.
-(setq use-package-ensure-function
-      (lambda (name &rest _)
-        (message "Ignoring ':ensure t' in '%s' config" name)))
-;; ...On the other hand, if the user has loaded `package', then we should assume
-;; they know what they're doing and restore the old behavior:
-(add-transient-hook! 'package-initialize
-  (when (eq use-package-ensure-function #'ignore)
-    (setq use-package-ensure-function #'use-package-ensure-elpa)))
+       :emacs
+       dired             ; making dired pretty [functional]
+       electric          ; smarter, keyword-based electric-indent
+       ;;ibuffer         ; interactive buffer management
+       undo              ; persistent, smarter undo for your inevitable mistakes
+       vc                ; version-control and Emacs, sitting in a tree
 
-(with-eval-after-load 'use-package-core
-  ;; `use-package' adds syntax highlighting for the `use-package' macro, but
-  ;; Emacs 26+ already highlights macros, so it's redundant.
-  (font-lock-remove-keywords 'emacs-lisp-mode use-package-font-lock-keywords)
+       :term
+       ;;eshell            ; the elisp shell that works everywhere
+       ;;shell             ; simple shell REPL for Emacs
+       ;;term              ; basic terminal emulator for Emacs
+       ;;vterm             ; the best terminal emulation in Emacs
 
-  ;; We define :minor and :magic-minor from the `auto-minor-mode' package here
-  ;; so we don't have to load `auto-minor-mode' so early.
-  (dolist (keyword '(:minor :magic-minor))
-    (setq use-package-keywords
-          (use-package-list-insert keyword use-package-keywords :commands)))
+       :checkers
+       syntax              ; tasing you for every semicolon you forget
+       ;;(spell +flyspell) ; tasing you for misspelling mispelling
+       ;;grammar           ; tasing grammar mistake every you make
 
-  (defalias 'use-package-normalize/:minor #'use-package-normalize-mode)
-  (defun use-package-handler/:minor (name _ arg rest state)
-    (use-package-handle-mode name 'auto-minor-mode-alist arg rest state))
+       :tools
+       ;;ansible
+       ;;biblio            ; Writes a PhD for you (citation needed)
+       ;;debugger          ; FIXME stepping through code, to help you add bugs
+       ;;direnv
+       ;;docker
+       ;;editorconfig      ; let someone else argue about tabs vs spaces
+       ;;ein               ; tame Jupyter notebooks with emacs
+       (eval +overlay)     ; run code, run (also, repls)
+       ;;gist              ; interacting with github gists
+       lookup              ; navigate your code and its documentation
+       ;;lsp               ; M-x vscode
+       magit             ; a git porcelain for Emacs
+       ;;make              ; run make tasks from Emacs
+       ;;pass              ; password manager for nerds
+       ;;pdf               ; pdf enhancements
+       ;;prodigy           ; FIXME managing external services & code builders
+       ;;rgb               ; creating color strings
+       ;;taskrunner        ; taskrunner for all your projects
+       ;;terraform         ; infrastructure as code
+       ;;tmux              ; an API for interacting with tmux
+       ;;tree-sitter       ; syntax and parsing, sitting in a tree...
+       ;;upload            ; map local to remote projects via ssh/ftp
 
-  (defalias 'use-package-normalize/:magic-minor #'use-package-normalize-mode)
-  (defun use-package-handler/:magic-minor (name _ arg rest state)
-    (use-package-handle-mode name 'auto-minor-mode-magic-alist arg rest state))
+       :os
+       (:if IS-MAC macos)  ; improve compatibility with macOS
+       ;;tty               ; improve the terminal Emacs experience
 
-  ;; HACK Fix `:load-path' so it resolves relative paths to the containing file,
-  ;;      rather than `user-emacs-directory'. This is a done as a convenience
-  ;;      for users, wanting to specify a local directory.
-  (defadvice! doom--resolve-load-path-from-containg-file-a (fn label arg &optional recursed)
-    "Resolve :load-path from the current directory."
-    :around #'use-package-normalize-paths
-    ;; `use-package-normalize-paths' resolves paths relative to
-    ;; `user-emacs-directory', so we change that.
-    (let ((user-emacs-directory
-           (or (and (stringp arg)
-                    (not (file-name-absolute-p arg))
-                    (ignore-errors (dir!)))
-               doom-emacs-dir)))
-      (funcall fn label arg recursed)))
+       :lang
+       ;;agda              ; types of types of types of types...
+       ;;beancount         ; mind the GAAP
+       ;;(cc +lsp)         ; C > C++ == 1
+       ;;clojure           ; java with a lisp
+       ;;common-lisp       ; if you've seen one lisp, you've seen them all
+       ;;coq               ; proofs-as-programs
+       ;;crystal           ; ruby at the speed of c
+       ;;csharp            ; unity, .NET, and mono shenanigans
+       ;;data              ; config/data formats
+       ;;(dart +flutter)   ; paint ui and not much else
+       ;;dhall
+       ;;elixir            ; erlang done right
+       ;;elm               ; care for a cup of TEA?
+       emacs-lisp        ; drown in parentheses
+       ;;erlang            ; an elegant language for a more civilized age
+       ;;ess               ; emacs speaks statistics
+       ;;factor
+       ;;faust             ; dsp, but you get to keep your soul
+       ;;fortran           ; in FORTRAN, GOD is REAL (unless declared INTEGER)
+       ;;fsharp            ; ML stands for Microsoft's Language
+       ;;fstar             ; (dependent) types and (monadic) effects and Z3
+       ;;gdscript          ; the language you waited for
+       ;;(go +lsp)         ; the hipster dialect
+       ;;(graphql +lsp)    ; Give queries a REST
+       ;;(haskell +lsp)    ; a language that's lazier than I am
+       ;;hy                ; readability of scheme w/ speed of python
+       ;;idris             ; a language you can depend on
+       ;;json              ; At least it ain't XML
+       ;;(java +lsp)       ; the poster child for carpal tunnel syndrome
+       ;;javascript        ; all(hope(abandon(ye(who(enter(here))))))
+       ;;julia             ; a better, faster MATLAB
+       ;;kotlin            ; a better, slicker Java(Script)
+       ;;latex             ; writing papers in Emacs has never been so fun
+       ;;lean              ; for folks with too much to prove
+       ;;ledger            ; be audit you can be
+       ;;lua               ; one-based indices? one-based indices
+       markdown          ; writing docs for people to ignore
+       ;;nim               ; python + lisp at the speed of c
+       ;;nix               ; I hereby declare "nix geht mehr!"
+       ;;ocaml             ; an objective camel
+       org               ; organize your plain life in plain text
+       ;;php               ; perl's insecure younger brother
+       ;;plantuml          ; diagrams for confusing people more
+       ;;purescript        ; javascript, but functional
+       ;;python            ; beautiful is better than ugly
+       ;;qt                ; the 'cutest' gui framework ever
+       ;;racket            ; a DSL for DSLs
+       ;;raku              ; the artist formerly known as perl6
+       ;;rest              ; Emacs as a REST client
+       ;;rst               ; ReST in peace
+       ;;(ruby +rails)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
+       ;;(rust +lsp)       ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
+       ;;scala             ; java, but good
+       ;;(scheme +guile)   ; a fully conniving family of lisps
+       sh                ; she sells {ba,z,fi}sh shells on the C xor
+       ;;sml
+       ;;solidity          ; do you need a blockchain? No.
+       ;;swift             ; who asked for emoji variables?
+       ;;terra             ; Earth and Moon in alignment for performance.
+       ;;web               ; the tubes
+       ;;yaml              ; JSON, but readable
+       ;;zig               ; C, but simpler
 
-  ;; Adds two keywords to `use-package' to expand its lazy-loading capabilities:
-  ;;
-  ;;   :after-call SYMBOL|LIST
-  ;;   :defer-incrementally SYMBOL|LIST|t
-  ;;
-  ;; Check out `use-package!'s documentation for more about these two.
-  (dolist (keyword '(:defer-incrementally :after-call))
-    (push keyword use-package-deferring-keywords)
-    (setq use-package-keywords
-          (use-package-list-insert keyword use-package-keywords :after)))
+       :email
+       ;;(mu4e +org +gmail)
+       ;;notmuch
+       ;;(wanderlust +gmail)
 
-  (defalias 'use-package-normalize/:defer-incrementally #'use-package-normalize-symlist)
-  (defun use-package-handler/:defer-incrementally (name _keyword targets rest state)
-    (use-package-concat
-     `((doom-load-packages-incrementally
-        ',(if (equal targets '(t))
-              (list name)
-            (append targets (list name)))))
-     (use-package-process-keywords name rest state)))
+       :app
+       ;;calendar
+       ;;emms
+       ;;everywhere        ; *leave* Emacs!? You must be joking
+       ;;irc               ; how neckbeards socialize
+       ;;(rss +org)        ; emacs as an RSS reader
+       ;;twitter           ; twitter client https://twitter.com/vnought
 
-  (defalias 'use-package-normalize/:after-call #'use-package-normalize-symlist)
-  (defun use-package-handler/:after-call (name _keyword hooks rest state)
-    (if (plist-get state :demand)
-        (use-package-process-keywords name rest state)
-      (let ((fn (make-symbol (format "doom--after-call-%s-h" name))))
-        (use-package-concat
-         `((fset ',fn
-                 (lambda (&rest _)
-                   (doom-log "use-package: lazy loading %s from %s" ',name ',fn)
-                   (condition-case e
-                       ;; If `default-directory' is a directory that doesn't
-                       ;; exist or is unreadable, Emacs throws up file-missing
-                       ;; errors, so we set it to a directory we know exists and
-                       ;; is readable.
-                       (let ((default-directory doom-emacs-dir))
-                         (require ',name))
-                     ((debug error)
-                      (message "Failed to load deferred package %s: %s" ',name e)))
-                   (when-let (deferral-list (assq ',name doom--deferred-packages-alist))
-                     (dolist (hook (cdr deferral-list))
-                       (advice-remove hook #',fn)
-                       (remove-hook hook #',fn))
-                     (delq! deferral-list doom--deferred-packages-alist)
-                     (unintern ',fn nil)))))
-         (let (forms)
-           (dolist (hook hooks forms)
-             (push (if (string-match-p "-\\(?:functions\\|hook\\)$" (symbol-name hook))
-                       `(add-hook ',hook #',fn)
-                     `(advice-add #',hook :before #',fn))
-                   forms)))
-         `((unless (assq ',name doom--deferred-packages-alist)
-             (push '(,name) doom--deferred-packages-alist))
-           (nconc (assq ',name doom--deferred-packages-alist)
-                  '(,@hooks)))
-         (use-package-process-keywords name rest state))))))
-
-
-
-;;
-;;; Macros
-
-(defvar doom-disabled-packages)
-(defmacro use-package! (name &rest plist)
-  "Declares and configures a package.
-
-This is a thin wrapper around `use-package', and is ignored if the NAME package
-is disabled by the user (with `package!').
-
-See `use-package' to see what properties can be provided. Doom adds support for
-two extra properties:
-
-:after-call SYMBOL|LIST
-  Takes a symbol or list of symbols representing functions or hook variables.
-  The first time any of these functions or hooks are executed, the package is
-  loaded.
-
-:defer-incrementally SYMBOL|LIST|t
-  Takes a symbol or list of symbols representing packages that will be loaded
-  incrementally at startup before this one. This is helpful for large packages
-  like magit or org, which load a lot of dependencies on first load. This lets
-  you load them piece-meal during idle periods, so that when you finally do need
-  the package, it'll load quicker.
-
-  NAME is implicitly added if this property is present and non-nil. No need to
-  specify it. A value of `t' implies NAME."
-  (declare (indent 1))
-  (unless (or (memq name doom-disabled-packages)
-              ;; At compile-time, use-package will forcibly load packages to
-              ;; prevent compile-time errors. However, if a Doom user has
-              ;; disabled packages you get file-missing package errors, so it's
-              ;; necessary to check for packages at compile time:
-              (and (bound-and-true-p byte-compile-current-file)
-                   (not (locate-library (symbol-name name)))))
-    `(use-package ,name ,@plist)))
-
-(defmacro use-package-hook! (package when &rest body)
-  "Reconfigures a package's `use-package!' block.
-
-This macro must be used *before* PACKAGE's `use-package!' block. Often, this
-means using it from your DOOMDIR/init.el.
-
-Under the hood, this uses use-package's `use-package-inject-hooks'.
-
-PACKAGE is a symbol; the package's name.
-WHEN should be one of the following:
-  :pre-init :post-init :pre-config :post-config
-
-WARNINGS:
-- The use of this macro is more often than not a code smell. Use it as last
-  resort. There is almost always a better alternative.
-- If you are using this solely for :post-config, stop! `after!' is much better.
-- If :pre-init or :pre-config hooks return nil, the original `use-package!''s
-  :init/:config block (respectively) is overwritten, so remember to have them
-  return non-nil (or exploit that to overwrite Doom's config)."
-  (declare (indent defun))
-  (unless (memq when '(:pre-init :post-init :pre-config :post-config))
-    (error "'%s' isn't a valid hook for use-package-hook!" when))
-  `(progn
-     (setq use-package-inject-hooks t)
-     (add-hook ',(intern (format "use-package--%s--%s-hook"
-                                 package
-                                 (substring (symbol-name when) 1)))
-               (lambda () ,@body)
-               'append)))
-
-;;; init.el ends here
+       :config
+       ;;literate
+       (default +bindings +smartparens))
