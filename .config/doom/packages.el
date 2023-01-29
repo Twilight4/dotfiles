68bc93e98eb1a7c5b3991547a963638bbffdbaff
Twilight4 <99445042+Twilight4@users.noreply.github.com>
2023-01-29 18:13:21 +0100

    Update packages.el

--- .config/doom/packages.el
+++ .config/doom/packages.el
@@ -48,3 +48,8 @@
 ;(unpin! pinned-package another-pinned-package)
 ;; ...Or *all* packages (NOT RECOMMENDED; will likely break things)
 ;(unpin! t)
+
+(package! calfw)
+(package! calfw-org)
+(package! org-auto-tangle)
+(package! org-web-tools)
