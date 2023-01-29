a0b9a2a15642d8db8db9f6bb3e9b04da2f1ebb78
Twilight4 <99445042+Twilight4@users.noreply.github.com>
2023-01-29 16:23:42 +0100

    Update init.el

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -43,8 +43,8 @@
        ;;neotree           ; a project drawer, like NERDTree for vim
        ophints           ; highlight the region an operation acts on
        (popup +defaults)   ; tame sudden yet inevitable temporary windows
-       tabs              ; a tab bar for Emacs
-       ;;treemacs          ; a project drawer, like neotree but cooler
+       ;;tabs              ; a tab bar for Emacs
+       treemacs          ; a project drawer, like neotree but cooler
        ;;unicode           ; extended unicode support for various languages
        (vc-gutter +pretty) ; vcs diff in the fringe
        vi-tilde-fringe   ; fringe tildes to mark beyond EOB
