8e835a591b45269bd299f98dc3ce51bbbab66607
Twilight4 <99445042+Twilight4@users.noreply.github.com>
2023-02-06 05:00:22 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -52,6 +52,15 @@ install-apps() {
     sudo pacman -U --noconfirm --needed /tmp/archlinux-logout-git-23.01-01-any.pkg.tar.zst
     npm install git-file-downloader # TODO: move the installed dir and add .bin to PATH
     
+    # gaming on linux
+    #sudo pacman --noconfirm -S lutris
+    #sudo pacman --noconfirm -S --needed wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls \
+    #mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error \
+    #lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo \
+    #sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama \
+    #ncurses lib32-ncurses ocl-icd lib32-ocl-icd libxslt lib32-libxslt libva lib32-libva gtk3 \
+    #lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader
+    
     # zsh as default terminal for user
     sudo chsh -s "$(which zsh)" "$(whoami)"
     
