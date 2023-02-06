c3967bd3bdaab7bb4e2419bae3d6c7b06d59cd82
Twilight4 <99445042+Twilight4@users.noreply.github.com>
2023-02-06 05:02:32 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -54,6 +54,7 @@ install-apps() {
     
     # gaming on linux
     #sudo pacman --noconfirm -S lutris
+    #sudo pacman --noconfirm -S --needed lib32-mesa vulkan-radeon lib32-vulkan-radeon vulkan-icd-loader lib32-vulkan-icd-loader
     #sudo pacman --noconfirm -S --needed wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls \
     #mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error \
     #lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo \
