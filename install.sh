221242a127eb309f9bad3d692ca73ae5a6ca936a
Twilight4 <99445042+Twilight4@users.noreply.github.com>
2023-02-09 14:09:31 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -127,7 +127,6 @@ install-dotfiles() {
     systemctl --user enable greenclip.service                                         # enable cliphistory daemon
     systemctl --user enable mpd.service                                               # mpd daemon
     systemctl --user enable psd.service                                               # profile sync daemon
-    #systemctl --user enable emacs.service                                             # enable emacs server daemon
     sudo systemctl enable vnstat.service                                              # network traffic monitor
     sudo systemctl enable ananicy.service                                             # enable ananicy daemon 
     sudo systemctl enable nohang-desktop.service                                      # enable nohang daemon
