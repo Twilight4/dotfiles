ee1d93d82df5532305760b9aaa655360bb779aad
Twilight4 <99445042+Twilight4@users.noreply.github.com>
2023-02-12 14:15:33 +0100

    Create config.py

---
+++ .config/qutebrowser/config.py
@@ -0,0 +1,35 @@
+# =============== Special Format Yanking (designed for org mode) ===========
+config.bind("<y><o>", "yank inline [[{url}][{title}]]")
+
+# ======================= External Open ===================
+config.bind("V", "hint links spawn " + os.environ["BROWSER"] + ' "{hint-url}"')
+config.bind("v", 'hint links spawn funnel "{hint-url}"')
+config.bind("\\", 'spawn dmenuhandler "{url}"')
+
+# ======================= Redline Insert Mode ============= {{{
+# Awesome way to open vim from qutebrowser
+c.editor.command = [
+    os.environ["TERMINAL"],
+    "-e",
+    os.environ["EDITOR"],
+    "-f",
+    "{file}",
+    "-c",
+    "normal {line}G{column0}1",
+]
+
+config.bind("<Ctrl-h>", "fake-key <Backspace>", "insert")
+config.bind("<Ctrl-a>", "fake-key <Home>", "insert")
+config.bind("<Ctrl-e>", "fake-key <End>", "insert")
+config.bind("<Ctrl-b>", "fake-key <Left>", "insert")
+config.bind("<Mod1-b>", "fake-key <Ctrl-Left>", "insert")
+config.bind("<Ctrl-f>", "fake-key <Right>", "insert")
+config.bind("<Mod1-f>", "fake-key <Ctrl-Right>", "insert")
+config.bind("<Ctrl-p>", "fake-key <Up>", "insert")
+config.bind("<Ctrl-n>", "fake-key <Down>", "insert")
+config.bind("<Mod1-d>", "fake-key <Ctrl-Delete>", "insert")
+config.bind("<Ctrl-d>", "fake-key <Delete>", "insert")
+config.bind("<Ctrl-w>", "fake-key <Ctrl-Backspace>", "insert")
+config.bind("<Ctrl-u>", "fake-key <Shift-Home><Delete>", "insert")
+config.bind("<Ctrl-k>", "fake-key <Shift-End><Delete>", "insert")
+config.bind("<Ctrl-x><Ctrl-e>", "open-editor", "insert")
