eaf6fcca34f4f6198a8ac71cd2ab4f4257e44ee9
Twilight4 <99445042+Twilight4@users.noreply.github.com>
2023-02-14 19:37:43 +0000

    Update cpu-temp.sh

--- .config/waybar/scripts/cpu-temp.sh
+++ .config/waybar/scripts/cpu-temp.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
+
 # Since string manipulations in bash can't be nested, temporary variables are necessary.
-
-a="$(acpi -t)"
-a="${a#*, }"
-a="${a% d*}"
-
-echo "$a°C"
+a="$(acpi -t)";a="${a#*, }";printf '%s°C\n' "${a% d*}"
