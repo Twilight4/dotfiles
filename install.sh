4a88a21d5e75e6800af7e82de84eb85efc3391b0
Twilight4 <99445042+Twilight4@users.noreply.github.com>
2023-02-20 08:18:02 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -126,10 +126,6 @@ install-dotfiles() {
     sudo chmod 755 "$XDG_CONFIG_HOME"/zsh/bash-scripts/*.sh
     sudo chmod 755 /opt/logseq-desktop
     
-    # (has to be after cloned dotfiles) - don't install doom emacs, if you change your mind then this is the package from aur: emacs-gcc-wayland-devel-bin
-    #git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.emacs.d
-    #~/.emacs.d/bin/doom install
-        
     # system services
     playerctld daemon                                                                 # if it doesn't work try installing volumectl
     systemctl --user enable greenclip.service                                         # enable cliphistory daemon
