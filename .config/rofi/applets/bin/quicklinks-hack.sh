10b63a95f687e04cb42dc94404ed2e6aa3921ed9
Twilight4 <electrolight071@gmail.com>
2023-03-04 20:04:25 +0100

    update

--- .config/rofi/applets/bin/quicklinks-hack.sh
+++ .config/rofi/applets/bin/quicklinks-hack.sh
@@ -22,7 +22,7 @@ elif [[ ( "$theme" == *'type-2'* ) || ( "$theme" == *'type-4'* ) ]]; then
 fi
 
 if [[ ( "$theme" == *'type-1'* ) || ( "$theme" == *'type-5'* ) ]]; then
-	efonts="JetBrains Mono Nerd Font 10"
+	efonts="JetBrains Mono Nerd Font 11"
 else
 	efonts="JetBrains Mono Nerd Font 28"
 fi
