11bf8a4b8a7b6256d3f9aa1815dfe6b266c43228
Twilight4 <electrolight071@gmail.com>
2023-03-05 10:12:46 +0100

    update

--- .config/waybar/scripts/vpn-check.sh
+++ .config/waybar/scripts/vpn-check.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ $(ip a s | grep tun0 | grep inet | awk '{print $2;}' | cut -d . -f2 | tr -d '\n' | wc -c) -eq 3 ]; then
+if [ $(ip a s | grep tun0 | grep inet | awk '{print $2;}' | cut -d . -f2 | tr -d '\n' | wc -c) -eq 2 ]; then
   echo "ON"
-elif [ $(ip a s | grep tun0 | grep inet | awk '{print $2;}' | cut -d . -f2 | tr -d '\n' | wc -c) -eq 1 ]; then
+elif [ $(ip a s | grep tun0 | grep inet | awk '{print $2;}' | cut -d . -f2 | tr -d '\n' | wc -c) -eq 0 ]; then
   echo "OFF"
 fi
