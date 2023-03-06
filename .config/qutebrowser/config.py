570f6fb4ad1cf79002b0cbb44bb2665e7d398404
Twilight4 <electrolight071@gmail.com>
2023-03-06 11:08:51 +0100

    update

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -90,7 +90,6 @@ config.bind("<Ctrl-d>", "fake-key <Delete>", "insert")
 config.bind("<Ctrl-w>", "fake-key <Ctrl-Backspace>", "insert")
 config.bind("<Ctrl-u>", "fake-key <Shift-Home><Delete>", "insert")
 config.bind("<Ctrl-k>", "fake-key <Shift-End><Delete>", "insert")
-config.bind("<Ctrl-x><Ctrl-e>", "open-editor", "insert")
 
 # Bindings for normal mode
 config.bind('M', 'hint links spawn mpv {hint-url}')
