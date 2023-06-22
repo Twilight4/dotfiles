ceb6f716812bb79d48a1cce814ed8cc3feecd3fe
Twilight4 <electrolight071@gmail.com>
2023-06-22 19:07:45 +0200

    update

--- install.sh
+++ install.sh
@@ -266,12 +266,6 @@ echo 'Post-Installation:
     sudo npm install git-file-downloader cli-fireplace git-stats
 - check if profile sync daemon is running
     psd p
-- Check if DBus service is launched (for kde connect)
-    gapplication launch org.gnome.Shell.Extensions.GSConnect
-    - if it returns error message then restart the DBus session
-    systemctl --user reload dbus-broker.service
-    - If you are on GNOME then to switch caps with esc issue command (it should be in dconf database)
-    gsettings set org.gnome.desktop.input-sources xkb-options "['caps:swapescape']"
 - uncomment last 2 lines in kitty.conf'
 }
 
