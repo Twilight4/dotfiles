25b7e2cf01bf32a1484b4c2648dc6100f2058c32
Twilight4 <electrolight071@gmail.com>
2023-06-30 12:29:35 +0200

    update

--- install.sh
+++ install.sh
@@ -211,9 +211,6 @@ install-dotfiles() {
     sudo sed -i 's/^timeout/# timeout/' /boot/loader/loader.conf 
     # Change data locale back to english
     sudo localectl set-locale LC_TIME=en_US.UTF8
-    # Enable Firewall blocking kdeconnect port
-    sudo firewall-cmd --permanent --zone=public --add-service=kdeconnect 
-    sudo firewall-cmd --reload
 
 # Hyprland desktop entry
 #sudo mv /tmp/dotfiles/hyprland.desktop /usr/share/wayland-sessions/hyprland.desktop
