ace8e7d3845b330378cb2e241902588f91d2eb92
Twilight4 <electrolight071@gmail.com>
2023-07-10 19:22:28 +0200

    update

--- install.sh
+++ install.sh
@@ -65,6 +65,8 @@ remove-distro-bloat() {
       btop
       cachyos-packageinstaller
       vim
+      meld
+      glances
       xdg-desktop-portal-gnome
       xdg-desktop-portal-gtk
       xdg-desktop-portal-wlr
