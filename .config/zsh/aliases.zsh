2c3a4bba252298c4a93640a5ed0c89d604f48b89
Twilight4 <electrolight071@gmail.com>
2023-08-05 16:00:34 +0200

    update

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -2,6 +2,7 @@
 
 # ls to lsd
 alias l='lsd --hyperlink=auto'
+alias cl='clear ; lsd -l --hyperlink=auto'
 alias ls='lsd -l --hyperlink=auto'
 alias la='lsd -lA --hyperlink=auto'
 alias lt='lsd --tree --hyperlink=auto'
@@ -12,14 +13,41 @@ alias lc='lsd -ltrh --hyperlink=auto'                # sort by date
 #alias ld='lsd -l --hyperlink=auto| grep '^d''        # directories only
 alias l.='lsd -A $* | grep "^\."'                    # list hidden files
 
-# list cheatsheets for tools
+# HOME dirs
+alias dw='cd "$HOME/downloads" ; clear'
+alias dt='cd "$HOME/desktop" ; clear'
+alias pc='cd "$HOME/pictures" ; clear'
+alias vd='cd "$HOME/videos" ; clear'
+alias dc='cd "$HOME/documents" ; clear'
+
+# Work dirs
+alias pt='cd "$HOME/desktop/pentest" ; clear'
+alias lpt='lsd --tree --hyperlink=auto ~/desktop/pentest'
+alias ws='cd "$HOME/workspace" ; clear'
+
+# Common usage
+alias r='cd $HOME ; clear'
+alias w='cd "$HOME/desktop/server" ; echo "$(hip) in $PWD" ; sudo python3 -m http.server 80'
+alias w2='cd "$HOME/desktop/server" ; echo "$(hip) in $PWD" ; sudo python3 -m http.server 8000'
+alias w3='ngrok http 4444'
+alias s="kitty +kitten ssh"
+alias v="nvim"
+alias e='exit'
+alias m='service postgresql start ; msfdb init ; msfconsole'
+alias ce='cheat --edit'
+
+# List cheatsheets for tools
 alias ltp='lsd --tree --hyperlink=auto ~/workspace/cheats/pentest'    # pentest tools
 alias ltw='lsd --tree --hyperlink=auto ~/workspace/cheats/web-app'    # web app tools
 alias ltr='lsd --tree --hyperlink=auto ~/workspace/cheats/red-team'   # red team tools
 alias ltf='lsd --tree --hyperlink=auto ~/workspace/cheats/forensics'  # forensics tools
 alias lto='lsd --tree --hyperlink=auto ~/workspace/cheats/other'      # other tools
 
-# aliases to modified commands
+# Blackarch repo packages
+alias blackall="sudo pacman -Sgg | grep blackarch | cut -d' ' -f2 | sort -u"  # list all available tools
+alias blackcat="sudo pacman -Sg | grep blackarch"                             # see the blackarch categories
+
+# Aliases to modified commands
 alias mkdir="mkdir -p"
 alias ping="prettyping -c 3"
 alias less="less -R"
@@ -53,27 +81,7 @@ alias icat='kitty +kitten icat'
 alias diff="kitty +kitten diff"
 alias gdiff="git difftool --no-symlinks --dir-diff"
 
-# HOME dirs
-alias dw='cd "$HOME/downloads" ; clear'
-alias dt='cd "$HOME/desktop" ; clear'
-alias pc='cd "$HOME/pictures" ; clear'
-alias vd='cd "$HOME/videos" ; clear'
-alias ws='cd "$HOME/workspace" ; clear'
-alias dc='cd "$HOME/documents" ; clear'
-
-# common usage
-alias s="kitty +kitten ssh"
-alias v="nvim"
-alias e='exit'
-alias m='service postgresql start ; msfdb init ; msfconsole'
-alias r='cd $HOME ; clear'
-alias w='cd "$HOME/desktop" ; echo "$(hip) in $PWD" ; sudo python3 -m http.server 80'
-alias ce='cheat --edit'
-alias cl='clear ; lsd -l --hyperlink=auto'
-alias w2='cd "$HOME/desktop" ; echo "$(hip) in $PWD" ; sudo python3 -m http.server 8000'
-alias w3='ngrok http 4444'
-
-# prettify help/cheat pages
+# Prettify help/cheat pages
 alias bathelp='bat --plain --language=help'
 c() {
   command cheat "$@" | bat --plain --language=help
@@ -85,7 +93,7 @@ h() {
     "$@" --help 2>&1 | bathelp
 }
 
-# rust replacements
+# Rust replacements
 alias cp='xcp'
 alias http='xh'                # curl replacement
 alias httpd='http --download'  # uses xh alias first if installed
@@ -98,7 +106,7 @@ alias ncdu="ncdu --color dark"
 alias df='duf'
 alias bat='bat --paging=never --theme OneHalfDark'
 
-# mpv
+# Mpv
 alias mpk='mpv --profile=sw-fast --vo=kitty'
 alias mpvadd='mpv --ytdl'
 alias mpvpl='mpv "$(yt-dlp -g --flat-playlist "$1")"'
@@ -106,67 +114,62 @@ alias mpa='mpv --no-video'
 alias mpapl='mpv --no-video "$(yt-dlp -g -x --audio-format mp3 --flat-playlist "$1")"'
 alias mpvcwd='mpv ./*.mp4'
 
-# trans
+# Trans
 alias tre='trans en:pl'
 alias trp='trans pl:en'
 
-# check vulnerabilities microcode
+# Check vulnerabilities microcode
 alias microcode='grep . /sys/devices/system/cpu/vulnerabilities/*'
 
-# recent installed packages
+# Recent installed packages
 alias rip="expac --timefmt='%Y-%m-%d %T' '%l\t%n %v' | sort | tail -200 | nl"
 alias riplong="expac --timefmt='%Y-%m-%d %T' '%l\t%n %v' | sort | tail -3000 | nl"
 alias big="expac -H M '%m\t%n' | sort -h | nl"
 alias gitpkg="pacman -Q | grep -i '\-git' | wc -l"
 
-# blackarch repo packages
-alias blackall="sudo pacman -Sgg | grep blackarch | cut -d' ' -f2 | sort -u"  # list all available tools
-alias blackcat="sudo pacman -Sg | grep blackarch"                             # see the blackarch categories
-
-# gpg
-# verify signature for isos
+# Gpg - verify signature for isos
 alias gpg-check="gpg2 --keyserver-options auto-key-retrieve --verify"
 alias fix-gpg-check="gpg2 --keyserver-options auto-key-retrieve --verify"
-# receive the key
+# Gpg - receive the key
 alias gpg-retrieve="gpg2 --keyserver-options auto-key-retrieve --receive-keys"
 alias fix-gpg-retrieve="gpg2 --keyserver-options auto-key-retrieve --receive-keys"
 alias fix-keyserver="[ -d ~/.gnupg ] || mkdir ~/.gnupg ; cp /etc/pacman.d/gnupg/gpg. conf ~/.gnupg/ ; echo 'done'"
 
-# hblock (stop tracking with hblock) - use unhblock to stop using hblock
+# Hblock (stop tracking with hblock) - use unhblock to stop using hblock
 alias unhblock="hblock -S none -D none"
 
-# systeminfo
+# Systeminfo
 alias probe="sudo -E hw-probe -all -upload"
 alias sysfailed="systemctl list-units --failed"
 alias hw="hwinfo --short"
 
-# count all files (recursively) in the current folder
+# Count all files (recursively) in the current folder
 alias cf="bash -c \"for t in files links directories; do echo \\\$(find . -  type \\\${t:0:1} | wc -l) \\\$t; done 2> /dev/null\""
 
-# show current network connections to the server
+# Show current network connections to the server
 alias ipview="netstat -anpl | grep :80 | awk {'print \$5'} | cut -d\":\" -f1 | sort  | uniq -c | sort -n | sed -e 's/^ *//' -e 's/ *\$//'"
 alias wlo1='echo $(ifconfig wlo1 | rg "inet " | cut -b 9- | cut  -d" " -f2)'
 alias tun0='echo $(ifconfig tun0 | rg "inet " | cut -b 9- | cut  -d" " -f2)'
  
-# show open ports
+# Show open ports
 alias openports='netstat -nape --inet'
 
-# aliases to show disk space and space used in a folder
+# Show disk space and space used in a folder
 alias diskspace="du -S | sort -n -r |less -R"
 alias folders='du -h --max-depth=1'
 alias folderssort='find . -maxdepth 1 -type d -print0 | xargs -0 du -sk | sort -rn'
 alias tree='tree -CAhF --dirsfirst'
 alias treed='tree -CAFd'
 
-# show all logs in /var/log
+# Show all logs in /var/log
 alias logs="sudo find /var/log -type f -exec file {} \; | grep 'text' | cut -d' ' -  f1 | sed -e's/:\$//g' | grep -v '[0-9]\$' | xargs tail -f"
 
-# usefull
+# Usefull
 alias ..="cd ../"
 alias ...="cd ../../"
 alias ....="cd ../../../"
 
-# git
+# Git
 alias ga="git add"
 alias gc="git commit"
 alias gp="git push"
@@ -176,7 +179,7 @@ alias gcm="git checkout master"
 alias gcs="git checkout stable"
 alias gcd="cloneit"
 
-# pacman and yay
+# Pacman and yay
 alias pacs="sudo pacman -S"                      # install package faster
 alias pacr="sudo pacman -Rns"                    # remove package faster
 alias pacf="sudo pacman -F"                      # search binary package faster
@@ -190,7 +193,7 @@ alias yaysyu="yay -Syu --noconfirm"              # update standard pkgs and AUR 
 alias fixpac="sudo rm /var/lib/pacman/db.lck"    # remove pacman lock
 alias cleanup="sudo pacman -Rns (pacman -Qtdq)"  # cleanup orphaned packages
 
-# quick access to config files
+# Quick access to config files
 alias zshrc="nvim ~/.config/zsh/.zshrc"
 alias zshenv="nvim ~/.config/zsh/.zshenv"
 alias zsh-scripts="nvim ~/.config/zsh/zsh-scripts"
@@ -204,15 +207,15 @@ alias red="nvim ~/workspace/dotfiles/.config/cheat/tools-charts/red-ad-seq"
 alias pen="nvim ~/workspace/dotfiles/.config/cheat/tools-charts/pen-seq"
 alias osint="nvim ~/workspace/dotfiles/.config/cheat/tools-charts/osint-seq"
 
-# colorize grep output
+# Colorize grep output
 alias grep="grep --color=auto"
 alias egrep="egrep --color=auto"
 alias fgrep="fgrep --color=auto"
 
-# amass config alias
+# Amass config alias
 alias Amass='amass enum -config ~/.config/amass/config.ini -d $1'
 
-# search running processes
+# Search running processes
 alias psa="ps auxf"
 alias psrg="ps aux | grep -v grep | grep -i -e VSZ -e"
 alias psmem="ps auxf | sort -nr -k 4"
