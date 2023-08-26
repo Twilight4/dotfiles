90ab2419955996f8575e37fa28b2980e902cc674
Twilight4 <electrolight071@gmail.com>
2023-08-26 18:44:50 +0200

    update

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -112,7 +112,6 @@ alias mpvadd='mpv --ytdl'
 alias mpvpl='mpv "$(yt-dlp -g --flat-playlist "$1")"'
 alias mpa='mpv --no-video'
 alias mpapl='mpv --no-video "$(yt-dlp -g -x --audio-format mp3 --flat-playlist "$1")"'
-alias mpvcwd='mpv ./*.mp4'
 
 # Trans
 alias tre='trans en:pl'
