d254730cb8a223ec7bcbbfe6c786ab0c63f842ca
Twilight4 <electrolight071@gmail.com>
2023-10-21 23:57:16 +0200

    update

--- .config/zsh/bash-scripts/monitor-setup.sh
+++ .config/zsh/bash-scripts/monitor-setup.sh
@@ -8,7 +8,7 @@ if [ -f "/tmp/toggle-gamemode" ]; then
     riverctl unmap normal Shift D
     riverctl unmap normal Shift W
     riverctl unmap normal Shift E
-    riverctl hide-cursor timeout 5000
+    #riverctl hide-cursor timeout 5000    # For some reason this command doesn't have any affect whatsoever, so if you don't want to hide cursor, uncomment this line in river init file
     riverctl hide-cursor when-typing enabled
 else
     touch "/tmp/toggle-gamemode"
@@ -16,7 +16,7 @@ else
     riverctl map normal Shift D spawn '~/.config/zsh/bash-scripts/s-d.sh'
     riverctl map normal Shift W spawn '~/.config/zsh/bash-scripts/s-w.sh'
     riverctl map normal Shift E spawn '~/.config/zsh/bash-scripts/s-e.sh'
-    riverctl hide-cursor timeout 0
+    #riverctl hide-cursor timeout 0
     riverctl hide-cursor when-typing disabled
     if (wlr-randr | grep -q "eDP-1" ); then
         enabled_line=$(wlr-randr | grep -n "eDP-1" | head -n 1 | cut -d ':' -f 1)
