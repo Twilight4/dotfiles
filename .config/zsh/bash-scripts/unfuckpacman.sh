f57f246911e5143ae0e2eca741f3d92b86ec390b
Twilight4 <electrolight071@gmail.com>
2023-10-27 18:37:45 +0200

    update

--- .config/zsh/bash-scripts/unfuckpacman.sh
+++ .config/zsh/bash-scripts/unfuckpacman.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-sudo rm -R /etc/pacman.d/gnupg
-sudo rm -R /root/.gnupg
-sudo rm /var/cache/pacman -rf
-sudo dirmngr </dev/null
+
+sudo rm -rf /etc/pacman.d/gnupg/
 sudo pacman-key --init
 sudo pacman-key --populate
-sudo pacman -Syyuu
+
+# CachyOS keyring
+sudo pacman-key --recv-keys F3B607488DB35A47 --keyserver keyserver.ubuntu.com
+sudo pacman-key --lsign-key F3B607488DB35A47
+# AthenaOS keyring
+sudo pacman-key --recv-keys A3F78B994C2171D5 --keyserver keys.openpgp.org
+sudo pacman-key --lsign A3F78B994C2171D5
+sudo pacman -Syy
+
+echo 'Done - Press enter to exit; read _"'
