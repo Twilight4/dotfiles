3dd88af48fb3a1e7804dfa18dbc4b42ee91e0bb4
Twilight4 <electrolight071@gmail.com>
2023-11-11 19:01:39 +0100

    update

--- .config/zsh/bash-scripts/unfuckpacman.sh
+++ .config/zsh/bash-scripts/unfuckpacman.sh
@@ -5,8 +5,16 @@ sudo pacman-key --init
 sudo pacman-key --populate
 
 # CachyOS keyring
-sudo pacman-key --recv-keys F3B607488DB35A47 --keyserver keyserver.ubuntu.com
-sudo pacman-key --lsign-key F3B607488DB35A47
+#sudo pacman-key --recv-keys F3B607488DB35A47 --keyserver keyserver.ubuntu.com
+#sudo pacman-key --lsign-key F3B607488DB35A47
+
+# Automated Installation of cachyos repositories
+wget https://mirror.cachyos.org/cachyos-repo.tar.xz
+tar xvf cachyos-repo.tar.xz && cd cachyos-repo
+sudo ./cachyos-repo.sh
+cd -
+rm -rf cachyos-repo
+
 # AthenaOS keyring
 sudo pacman-key --recv-keys A3F78B994C2171D5 --keyserver keys.openpgp.org
 sudo pacman-key --lsign A3F78B994C2171D5
