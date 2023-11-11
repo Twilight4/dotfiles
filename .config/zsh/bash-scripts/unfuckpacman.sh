fa9320ea0c35f05806a104dcfaa994546547e5b5
Twilight4 <electrolight071@gmail.com>
2023-11-11 19:13:06 +0100

    update

--- .config/zsh/bash-scripts/unfuckpacman.sh
+++ .config/zsh/bash-scripts/unfuckpacman.sh
@@ -13,7 +13,7 @@ wget https://mirror.cachyos.org/cachyos-repo.tar.xz
 tar xvf cachyos-repo.tar.xz && cd cachyos-repo
 sudo ./cachyos-repo.sh
 cd -
-rm -rf cachyos-repo
+rm -rf cachyos-repo cachyos-repo.tar.xz
 
 # AthenaOS keyring
 sudo pacman-key --recv-keys A3F78B994C2171D5 --keyserver keys.openpgp.org
