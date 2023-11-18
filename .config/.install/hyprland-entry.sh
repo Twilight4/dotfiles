ac75d57eb2648f705aae2240ec3dfec7380a96f0
Twilight4 <electrolight071@gmail.com>
2023-11-18 20:22:34 +0100

    update

--- .config/.install/hyprland-entry.sh
+++ .config/.install/hyprland-entry.sh
@@ -3,15 +3,13 @@
 read -p "Do you want to create a Hyprland desktop entry? (y/n): " create_entry_choice
 
 if [[ "$create_entry_choice" =~ ^[Yy]$ ]]; then
-    if command -v hyprland >/dev/null; then
+    if command -v Hyprland >/dev/null; then
         echo "Creating Hyprland desktop entry..."
-
-        hypr_script=$(command -v hyprland)
 
         echo "[Desktop Entry]
         Name=Hyprland
-        Comment=hyprland
-        Exec=\"$hypr_script\"   # IF CRASHES TRY: bash -c \"$hypr_script\"
+        Comment=Highly customizable dynamic tiling Wayland compositor that doesn't sacrifice on its looks. 
+        Exec=Hyprland
         Type=Application" | sudo tee /usr/share/wayland-sessions/hyprland.desktop > /dev/null
 
         echo "Hyprland desktop entry created."
