324f7e104ea6580870bdaf1780315ca3909e0d8d
Twilight4 <electrolight071@gmail.com>
2023-11-19 17:25:06 +0100

    update

---
+++ .config/hypr/term-scripts/tm-open-all.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+~/.config/hypr/term-scripts/tm-webcam.sh
+~/.config/hypr/term-scripts/tm-musikcube.sh
+~/.config/hypr/term-scripts/tm-cava.sh
+~/.config/hypr/term-scripts/tm-clock.sh
+~/.config/hypr/term-scripts/tm-pipes.sh
+~/.config/hypr/term-scripts/tm-cmatrix.sh
+~/.config/hypr/term-scripts/tm-neofetch.sh
+~/.config/hypr/term-scripts/tm-asciiquarium.sh
+~/.config/hypr/term-scripts/tm-fireplace.sh
