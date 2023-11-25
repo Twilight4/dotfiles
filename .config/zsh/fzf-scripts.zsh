0bb38430a3d66e5c6d7119a3a31978dd352fba26
Twilight4 <electrolight071@gmail.com>
2023-11-25 22:26:55 +0100

    update

--- .config/zsh/fzf-scripts.zsh
+++ .config/zsh/fzf-scripts.zsh
@@ -195,15 +195,6 @@ ftrack() {
     [ -n "$file" ] && emacsclient -nw "$file"
 }
 
-# Find in File using ripgrep
-fif() {
-  if [ ! "$#" -gt 0 ]; then return 1; fi
-  rg --files-with-matches --no-messages "$1" \
-      | fzf --preview "highlight -O ansi -l {} 2> /dev/null \
-      | rg --colors 'match:bg:yellow' --ignore-case --pretty --context 10 '$1' \
-      || rg --ignore-case --pretty --context 10 '$1' {}"
-}
-
 # Find in file using ripgrep-all
 fifa() {
     if [ ! "$#" -gt 0 ]; then return 1; fi
