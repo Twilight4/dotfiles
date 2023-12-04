8f7b2b27d33c7d2aa802405901385e0056ad59e4
Twilight4 <electrolight071@gmail.com>
2023-12-04 15:10:36 +0100

    update

--- install.sh
+++ install.sh
@@ -54,6 +54,8 @@ source .config/.install/remove-bloat.sh
 source .config/.install/hyprland-packages.sh
 source .config/.install/install-packages.sh
 source .config/.install/enable-services.sh
+source .config/.install/qemu.sh
+source .config/.install/bluetooth.sh
 source .config/.install/set-user-groups.sh
 source .config/.install/wallpaper.sh
 source .config/.install/display-manager.sh
