11e00e6e97157b70420d06656fe2f54b62010073
Twilight4 <electrolight071@gmail.com>
2023-12-11 11:17:29 +0100

    update

--- .config/.install/button-layout.sh
+++ .config/.install/button-layout.sh
@@ -6,6 +6,7 @@ cat <<"EOF"
 | '_ \| | | | __| __/ _ \| '_ \/ __|
 | |_) | |_| | |_| || (_) | | | \__ \
 |_.__/ \__,_|\__|\__\___/|_| |_|___/
+
 EOF
                                     
 read -p "Do you want to remove the GTK button layout? (y/n): " update_layout_choice
@@ -21,8 +22,6 @@ if [[ "$update_layout_choice" =~ ^[Yy]$ ]]; then
     if [ "$current_button_layout" != "$desired_button_layout" ]; then
         # If they don't match, update the button layout using the gsettings command
         gsettings set org.gnome.desktop.wm.preferences button-layout "$desired_button_layout"
-        # Enable transparency in emacs - will move it later
-        sed -i '/;(add-to-list.*For all new frames henceforth/s/;//' your_file_name
         echo "Button layout has been updated."
     else
         # If they match, display a message indicating that the value is already as desired
