8b3437d48ff6f2a61e7420d6b7a89273c7ac0dc1
Twilight4 <electrolight071@gmail.com>
2024-01-02 20:42:16 +0100

    update

--- .config/.install/final-message.sh
+++ .config/.install/final-message.sh
@@ -14,6 +14,7 @@ echo '    sudo npm install git-file-downloader cli-fireplace git-stats'
 echo "Check if profile sync daemon is running:"
 echo '    psd p'
 echo "Uncomment last 2 lines in kitty.conf"
+echo "If gparted doesn't run try: xhost si:localuser:root"
 echo "Import the ~/.config/superProductivity/config.json in: Settings > Sync & Export > Import From File"
 echo "Add pub key to github: Settings > SSH > New:"
 echo '    ssh-keygen -t ed25519 -C "your_email@example.com"'
