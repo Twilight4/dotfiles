3f3b7c25c660c6dd4ff64a7a2926d4cbf3e59d48
Twilight4 <electrolight071@gmail.com>
2024-01-30 00:55:26 +0100

    update

--- .config/rofi/applets/bin/clipboard.sh
+++ .config/rofi/applets/bin/clipboard.sh
@@ -4,9 +4,7 @@ theme="launcher/style.rasi"
 dir="$HOME/.config/rofi"
 
 if [[ "$XDG_CURRENT_DESKTOP" == "Hyprland" ]]; then
-	cliphist list | rofi -p \
-		pick -dmenu -i -theme \
-		"$dir/$theme" -lines 10 | cliphist decode | wl-copy
+	cliphist list | rofi -p pick -dmenu -i -theme "$dir/$theme" -lines 10 | cliphist decode | wl-copy
 else
-  exit
+	exit
 fi
