3e16dbc2331f90b928ea61854a0277df996427c9
Twilight4 <electrolight071@gmail.com>
2024-02-05 19:47:20 +0100

    update

--- .config/zsh/scripts.zsh
+++ .config/zsh/scripts.zsh
@@ -68,20 +68,6 @@ diffd() {
   command diff -r -u "$@" | diff-so-fancy
 }
 
-# Add Vi text-objects for brackets and quotes
-autoload -Uz select-bracketed select-quoted
-zle -N select-quoted
-zle -N select-bracketed
-for km in viopp visual; do
-  bindkey -M $km -- '-' vi-up-line-or-history
-  for c in {a,i}${(s..)^:-\'\"\`\|,./:;=+@}; do
-    bindkey -M $km $c select-quoted
-  done
-  for c in {a,i}${(s..)^:-'()[]{}<>bB'}; do
-    bindkey -M $km $c select-bracketed
-  done
-done
-
 # Display width / height of an image
 imgsize() {
     local width=$(identify -format "%w" "$1")> /dev/null
