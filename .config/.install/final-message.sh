9f93076a7c68b3b499fcd93b5516a48d645dcf4b
Twilight4 <electrolight071@gmail.com>
2024-02-17 12:16:53 +0100

    update

--- .config/.install/final-message.sh
+++ .config/.install/final-message.sh
@@ -10,10 +10,14 @@ echo '    sudo npm install git-file-downloader cli-fireplace git-stats'
 echo "3. Check if profile sync daemon is running:"
 echo '    psd p'
 echo "4. Set up browser settings"
-echo "5. Import the ~/.config/superProductivity/config.json in: Settings > Sync & Export > Import From File"
-echo "6. Add pub key to github: Settings > SSH > New:"
+echo "5. Customize GRUB to disable useless options:"
+echo '    GRUB_DISABLE_RECOVERY=true'
+echo '    GRUB_DISABLE_SUBMENU=y'
+echo '    grubup'
+echo "6. Import the ~/.config/superProductivity/config.json in: Settings > Sync & Export > Import From File"
+echo "7. Add pub key to github: Settings > SSH > New:"
 echo '    ssh-keygen -t ed25519 -C "your_email@example.com"'
-echo "7. Clone relevant repos via SSH:"
+echo "8. Clone relevant repos via SSH:"
 echo '    git clone --depth 1 git@github.com:Twilight4/dotfiles.git ~/desktop/workspace'
 echo '    git clone --depth 1 git@github.com:Twilight4/org.git ~/documents'
 echo ""
