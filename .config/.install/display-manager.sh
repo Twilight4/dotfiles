3571322cb6cc890783416838e4d0a1fa714f9202
Twilight4 <electrolight071@gmail.com>
2024-02-17 13:07:14 +0100

    update

--- .config/.install/display-manager.sh
+++ .config/.install/display-manager.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ $disman == 1 ]; then
-cat <<"EOF"
+	cat <<"EOF"
 ____  _           _               __  __                                   
 |  _ \(_)___ _ __ | | __ _ _   _  |  \/  | __ _ _ __   __ _  __ _  ___ _ __ 
 | | | | / __| '_ \| |/ _` | | | | | |\/| |/ _` | '_ \ / _` |/ _` |/ _ \ '__|
@@ -10,25 +10,26 @@ ____  _           _               __  __
             |_|            |___/                            |___/
 EOF
 
-    while true; do
-        read -p "Do you want to install SDDM with theme (Yy/Nn): " yn
-        case $yn in
-            [Yy]* )
-				_installPackagesParu sddm-git
-				_installPackagesParu sddm-theme-astronaut
+	while true; do
+		read -p "Do you want to install SDDM with theme (Yy/Nn): " yn
+		case $yn in
+		[Yy]*)
+			_installPackagesParu sddm-git
+			_installPackagesParu sddm-theme-astronaut
 
-				echo "Creating /etc/sddm.conf file..."
-                
-                mv /etc/sddm.conf /etc/sddm.conf.bak
-				curl -LJO https://raw.githubusercontent.com/Twilight4/arch-setup/main/config-files/sddm.conf && sudo mv sddm.conf /etc/sddm.conf
+			echo "Creating /etc/sddm.conf file..."
+			mv /etc/sddm.conf /etc/sddm.conf.bak
+			curl -LJO https://raw.githubusercontent.com/Twilight4/arch-setup/main/config-files/sddm.conf && sudo mv sddm.conf /etc/sddm.conf
 
-				echo "/etc/sddm.conf file created."
-            break;;
-            [Nn]* )
-                echo "SDDM setup skipped."
-            break;;
-            * ) echo "Please answer yes or no.";;
-        esac
-    done
-    echo ""
+			echo "/etc/sddm.conf file created."
+			break
+			;;
+		[Nn]*)
+			echo "SDDM setup skipped."
+			break
+			;;
+		*) echo "Please answer yes or no." ;;
+		esac
+	done
+	echo ""
 fi
