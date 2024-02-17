4e228e3f25fc0604800a6dde65b3857d4104407c
Twilight4 <electrolight071@gmail.com>
2024-02-17 13:07:43 +0100

    update

--- .config/.install/hyprland-entry.sh
+++ .config/.install/hyprland-entry.sh
@@ -3,16 +3,14 @@
 read -p "Do you want to create a Hyprland desktop entry? (y/n): " create_entry_choice
 
 if [[ "$create_entry_choice" =~ ^[Yy]$ ]]; then
-    if command -v Hyprland >/dev/null; then
-        echo "Creating Hyprland desktop entry..."
-
-        sudo rm -v /usr/share/wayland-sessions/*
+	if command -v Hyprland >/dev/null; then
+		echo "Creating Hyprland desktop entry..."
+		sudo rm -v /usr/share/wayland-sessions/*
 		curl -LJO https://raw.githubusercontent.com/Twilight4/arch-setup/main/config-files/hyprland.desktop && sudo mv hyprland.desktop /usr/share/wayland-sessions/
-
-        echo "Hyprland desktop entry created."
-    else
-        echo "Hyprland is not installed."
-    fi
+		echo "Hyprland desktop entry created."
+	else
+		echo "Hyprland is not installed."
+	fi
 else
-    echo "Creation of Hyprland desktop entry canceled by user."
+	echo "Creation of Hyprland desktop entry canceled by user."
 fi
