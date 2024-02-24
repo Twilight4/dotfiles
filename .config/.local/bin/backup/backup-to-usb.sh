23b61a808a5f0aa6da7ea74cca837664cc2d9f6a
Twilight4 <electrolight071@gmail.com>
2024-02-24 21:39:54 +0100

    update

--- .config/.local/bin/backup/backup-to-usb.sh
+++ .config/.local/bin/backup/backup-to-usb.sh
@@ -3,13 +3,10 @@
 # backup home to usb
 rsync -aAXvz --ignore-errors --delete --progress \
 	--exclude downloads \
-	--exclude documents/BlackmagicDesign \
-	--exclude GPUCache \
 	--exclude music \
 	--exclude torrents \
 	--exclude videos \
 	--exclude .config \
 	--exclude .cache \
-	--exclude .dbus \
 	--exclude .floorp \
 	/home/$USER /mnt/usb
