22476a653b25ba563792e5bfe8ca5349e1a5b693
Twilight4 <electrolight071@gmail.com>
2024-03-15 17:15:27 +0100

    update

--- install.sh
+++ install.sh
@@ -9,12 +9,11 @@ clear
 
 mode="live"
 if [ ! -z $1 ]; then
-    mode="dev"
-    echo "IMPORTANT: DEV MODE ACTIVATED. "
-    echo "Existing dotfiles folder will not be modified."
-    echo "Symbolic links will not be created."
+	mode="dev"
+	echo "IMPORTANT: DEV MODE ACTIVATED. "
+	echo "Existing dotfiles folder will not be modified."
+	echo "Symbolic links will not be created."
 fi
-
 
 ################
 # Presentation #
@@ -38,7 +37,6 @@ echo "${YELLOW} Please make sure that your system and your packages are up to da
 echo "${YELLOW} This script will delete all your configuration files."
 echo "${YELLOW} Use at your own risk."
 echo ""
-
 
 ################
 # Installation #
@@ -64,7 +62,7 @@ source .config/.install/install-dotfiles.sh
 source .config/.install/hyprland-entry.sh
 source .config/.install/seclists.sh
 source .config/.install/auto-cpufreq.sh
-source .config/.install/systemd-boot.sh
+#source .config/.install/systemd-boot.sh
 source .config/.install/locales.sh
 source .config/.install/nnn.sh
 source .config/.install/supergfxd.sh
