975c75e747b4037dfda829602cb1f4897217f87b
Twilight4 <electrolight071@gmail.com>
2024-03-15 17:41:33 +0100

    update

--- .config/.install/final-message.sh
+++ .config/.install/final-message.sh
@@ -5,18 +5,16 @@ echo "DONE, Please logout and reboot your system."
 echo "------------------------------ AFTER REBOOT ------------------------------"
 echo "1. Check Default Network for virt-manager status"
 echo "    sudo virsh net-list --all"
-echo "2. Install more packages:"
-echo '    npm i -g git-stats'
-echo "3. Check if profile sync daemon is running:"
+echo "2. Check if profile sync daemon is running:"
 echo '    psd p'
-echo "4. Configure browser settings"
-echo "5. Import the ~/.config/superProductivity/config.json in: Settings > Sync & Export > Import From File"
-echo "6. Add pub key to github: Settings > SSH > New:"
+echo "3. Configure browser settings"
+echo "4. Import the ~/.config/superProductivity/config.json in: Settings > Sync & Export > Import From File"
+echo "5. Add pub key to github: Settings > SSH > New:"
 echo '    ssh-keygen -t ed25519'
-echo "7. Clone relevant repos via SSH:"
+echo "6. Clone relevant repos via SSH:"
 echo '    git clone --depth 1 git@github.com:Twilight4/dotfiles.git ~/desktop/workspace/dotfiles'
 echo '    git clone --depth 1 git@github.com:Twilight4/org.git ~/documents/org'
-echo "8. Set waybar and pywal theme:"
+echo "7. Set waybar and pywal theme:"
 echo "    SUPER+ALT+B"
 echo "    SUPER+CTRL+T - M4LW Blur Colored"
 echo ""
