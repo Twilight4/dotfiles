2be31372be3aa17ca6901891e1f0bd5e963d654a
Twilight4 <electrolight071@gmail.com>
2024-03-19 12:34:41 +0100

    update

--- .config/.install/final-message.sh
+++ .config/.install/final-message.sh
@@ -15,4 +15,5 @@ echo "6. Clone relevant repos via SSH:"
 echo '    git clone --depth 1 git@github.com:Twilight4/dotfiles.git ~/desktop/workspace/dotfiles'
 echo '    git clone --depth 1 git@github.com:Twilight4/org.git ~/documents/org'
 echo "7. Install more tools from arch-setup/tools-installation directory"
+echo "8. If the waybar workspaces icons are not in proper sequence, in modules.json change the numbers temporarily and restart waybar again, they will flal in place"
 echo ""
