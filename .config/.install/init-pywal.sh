1b5793bceb2ea04d0ca8d74176b7614ad84e0d3f
Twilight4 <electrolight071@proton.me>
2024-03-22 15:41:42 +0100

    update

--- .config/.install/init-pywal.sh
+++ .config/.install/init-pywal.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 _installSymLink wal ~/.config/wal ~/desktop/workspace/dotfiles/wal/ ~/.config
-wal -i ~/pictures/wallpapers/default.jpg
+wal -i ~/pictures/wallpapers/default.jpg -s -t
 echo "Pywal and templates initiated!"
 echo ""
