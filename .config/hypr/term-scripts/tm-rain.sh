8178909475eb73d468fc52cc814904cc127600b6
Twilight4 <electrolight071@proton.me>
2024-03-23 14:10:22 +0100

    update

--- .config/hypr/term-scripts/tm-rain.sh
+++ .config/hypr/term-scripts/tm-rain.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# Function to check if 'footclient' process is running
-is_foot_running() {
+# Function to check if 'kitty' process is running
+is_kitty_running() {
 	hyprctl clients | grep 'rain' >/dev/null
 }
 
 # Focus on ws "7" and spawn footclient if it's not running, else only focus
 if ! is_foot_running; then
     # Spawn 'footclient' only if it was not running
-    hyprctl dispatch exec 'footclient -a rain -e ~/.config/.local/bin/rain.sh'
+    hyprctl dispatch exec 'kitty -1 --class rain -e ~/.config/zsh/bash-scripts/rain'
 else
     hyprctl dispatch focuswindow '^rain$'
 fi
