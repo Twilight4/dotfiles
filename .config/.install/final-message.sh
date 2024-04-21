bdd793d0ee58d24f6b1e13dfe38616294138650e
Twilight4 <electrolight071@proton.me>
2024-04-21 13:58:07 +0200

    update

--- .config/.install/final-message.sh
+++ .config/.install/final-message.sh
@@ -9,11 +9,13 @@ echo "2. Configure browser settings"
 echo "3. Import the ~/.config/superProductivity/config.json in: Settings > Sync & Export > Import From File"
 echo "4. Import your OpenAI API key for emacs in '~/.authinfo':"
 echo '    machine api.openai.com login apikey password <TOKEN_HERE>'
-echo "4. Add pub key to github: Settings > SSH > New:"
+echo '5. Copy ai prompts to emacs dir to source them:'
+echo '    cp ~/desktop/workspace/dotfiles/.config/ai-prompts ~/.cache/emacs'
+echo "6. Add pub key to github: Settings > SSH > New:"
 echo '    ssh-keygen -t ed25519'
-echo "5. Clone relevant repos via SSH:"
+echo "7. Clone relevant repos via SSH:"
 echo '    git clone --depth 1 git@github.com:Twilight4/dotfiles.git ~/desktop/workspace/dotfiles'
 echo '    git clone --depth 1 git@github.com:Twilight4/org.git ~/documents/org'
 echo '    git clone --depth 1 git@github.com:Twilight4/cheats.git ~/.config/cheat'
-echo "6. Install more tools from arch-setup/tools-installation directory"
+echo "8. Install more tools from arch-setup/tools-installation directory"
 echo ""
