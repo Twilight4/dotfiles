ca4d08ffef83e0b4c1332f9e3d550de45032c446
Twilight4 <electrolight071@proton.me>
2024-05-22 23:30:25 +0200

    update

--- .config/.local/bin/backup/backup-to-usb.sh
+++ .config/.local/bin/backup/backup-to-usb.sh
@@ -4,9 +4,7 @@
 rsync -aAXvz --ignore-errors --delete --progress \
 	--exclude downloads \
 	--exclude music \
-	--exclude torrents \
 	--exclude videos \
 	--exclude .config \
 	--exclude .cache \
-	--exclude .floorp \
 	/home/$USER /mnt/usb
