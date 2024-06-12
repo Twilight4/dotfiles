5044cb478f935fc730bd37273e8b64de06dae93f
Twilight4 <electrolight071@gmail.com>
2024-06-12 09:04:20 +0200

    update

--- .config/.install/zsh.sh
+++ .config/.install/zsh.sh
@@ -27,32 +27,7 @@ if [ ! -f "$zshenv_file" ]; then
 	echo "Creating $zshenv_file..."
 	sudo echo "$line_to_append" | sudo tee -a "$zshenv_file"
 	echo "$zshenv_file created."
+  source ~/.config/zsh/.zshrc
 else
 	echo "$zshenv_file already exists."
-fi
-
-# Neovim Installation Script
-# Ask user if they want to install LazyVim
-read -p "Do you want to install LazyVim? (y/n): " install_lazyvim
-if [[ $install_lazyvim =~ ^[Yy]$ ]]; then
-	# Backup current Neovim files
-	echo "Backing up current Neovim files..."
-	mkdir -p ~/.config/nvim.bak
-	mv ~/.config/nvim/* ~/.config/nvim.bak 2>/dev/null
-	mv ~/.local/share/nvim ~/.local/share/nvim.bak 2>/dev/null
-	mv ~/.local/state/nvim ~/.local/state/nvim.bak 2>/dev/null
-	mv ~/.cache/nvim ~/.cache/nvim.bak 2>/dev/null
-
-	# Clone the lazyvim starter
-	echo "Cloning LazyVim starter..."
-	git clone https://github.com/LazyVim/starter ~/.config/nvim
-
-	# Remove the .git folder
-	echo "Removing .git folder..."
-	rm -rf ~/.config/nvim/.git
-
-	echo "LazyVim installation completed successfully."
-	echo "Make sure to run ':checkhealth' after installation for additional setup."
-else
-	echo "Skipping LazyVim installation."
 fi
