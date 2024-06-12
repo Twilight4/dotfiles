717a92cc61811044ebdc39ac65480a806a6c033c
Twilight4 <electrolight071@gmail.com>
2024-06-12 11:38:59 +0200

    update

--- .config/.install/install-kali-packages.sh
+++ .config/.install/install-kali-packages.sh
@@ -3,3 +3,4 @@
 # Install base packages
 sudo apt install libayatana-appindicator3-1 gir1.2-ayatanaappindicator3-0.1 lsd swaybg wdisplays ripgrep silversearcher-ag irqbalance acpi emacs profile-sync-daemon dunst translate-shell duf speedtest-cli gnome-weather gnome-keyring cpufetch fd-find trash-cli linux-cpupower mingw-w64 zathura grc poppler-utils gnome-maps wf-recorder thefuck libsecret-tools chafa pipewire pipewire-alsa pipewire-audio pipewire-jack pipewire-pulse wireplumber gtk2-engines-murrine
 sudo pip3 install pywal
+sudo apt purge sway-notification-center fdclone chromium
