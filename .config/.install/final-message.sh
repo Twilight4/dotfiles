c0c033ff6dd87cdf78be7906a83c90801d128040
Twilight4 <electrolight071@gmail.com>
2024-06-19 19:29:07 +0200

    update

--- .config/.install/final-message.sh
+++ .config/.install/final-message.sh
@@ -3,19 +3,16 @@
 # Post-install message
 echo "DONE, Please logout and reboot your system."
 echo "------------------------------ AFTER REBOOT ------------------------------"
-echo "1. Check if profile sync daemon is running:"
-echo '    psd p'
-echo "2. Configure browser settings"
-echo "3. Import the ~/.config/superProductivity/config.json in: Settings > Sync & Export > Import From File"
-echo "4. Import your OpenAI API key for emacs in '~/.authinfo':"
+echo "1. Import browser bookmarks"
+echo "2. Import your OpenAI API key for emacs in '~/.authinfo':"
 echo '    machine api.openai.com login apikey password <TOKEN_HERE>'
-echo '5. Copy ai prompts to emacs dir to source them:'
+echo '3. Copy ai prompts to emacs dir to source them:'
 echo '    cp ~/desktop/workspace/dotfiles/.config/ai-prompts ~/.cache/emacs'
-echo "6. Add pub key to github: Settings > SSH > New:"
+echo "4. Add pub key to github: Settings > SSH > New:"
 echo '    ssh-keygen -t ed25519'
-echo "7. Clone relevant repos via SSH:"
-echo '    git clone --depth 1 git@github.com:Twilight4/dotfiles.git ~/desktop/workspace/dotfiles'
-echo '    git clone --depth 1 git@github.com:Twilight4/org.git ~/documents/org'
-echo '    git clone --depth 1 git@github.com:Twilight4/cheats.git ~/.config/cheat'
-echo "8. Install more tools from debian-setup/tools-installation directory"
+echo "5. Clone relevant repos via SSH:"
+echo '    git clone git@github.com:Twilight4/dotfiles.git ~/desktop/workspace/dotfiles'
+echo '    git clone git@github.com:Twilight4/debian-setup.git ~/desktop/workspace/debian-setup'
+echo '    git clone git@github.com:Twilight4/org.git ~/documents/org'
+echo '    git clone git@github.com:Twilight4/cheats.git ~/.config/cheat'
 echo ""
