8658320408ad96234f4734ff8d056a40d0629790
Twilight4 <electrolight071@gmail.com>
2024-06-26 18:02:59 +0200

    update

---
+++ .config/.local/bin/cp-target.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Check if the file exists
+if [ -f ~/.nobility/vars/RHOST ]; then
+  # Check if the file is empty
+  if [ -s ~/.nobility/vars/RHOST ]; then
+    # Read the content of the file
+    RHOST_CONTENT=$(cat ~/.nobility/vars/RHOST)
+    # Use notify-send to display the content
+    notify-send -t 3000 "RHOST IP Address:" "$RHOST_CONTENT"
+    echo "$RHOST_CONTENT" | wl-copy
+  else
+    # Use notify-send to display a message if the file is empty
+    notify-send -t 3000 "RHOST is empty"
+  fi
+else
+  # Use notify-send to display an error message if the file doesn't exist
+  notify-send -t 3000 "File ~/.nobility/vars/RHOST does not exist."
+fi
