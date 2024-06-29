cd150baf2c03a2799258a18f699a2707a13a13cc
Twilight4 <electrolight071@gmail.com>
2024-06-29 16:43:50 +0200

    update

--- .config/.install/zsh.sh
+++ .config/.install/zsh.sh
@@ -39,3 +39,34 @@ if [ -f "$zshenv_file" ]; then
 else
 	echo "Error. Something went wrong."
 fi
+
+# Installing NVChad
+# Prompt user for input
+read -p "Do you want to install NVChad configuration (y/n)? " answer
+
+# Validate user input
+if [[ "$answer" != "y" && "$answer" != "Y" ]]; then
+    echo "Operation cancelled by the user."
+    exit 0
+fi
+
+# Remove existing NVChad configuration if it exists
+if [[ -d "$HOME/.config/nvim" ]]; then
+    rm -rf "$HOME/.config/nvim"
+    echo "Existing NVChad configuration removed."
+fi
+
+# Clone the NVChad repository
+if git clone https://github.com/NvChad/starter "$HOME/.config/nvim"; then
+    echo "NVChad configuration cloned successfully."
+else
+    echo "Failed to clone NVChad repository."
+    exit 1
+fi
+
+# Copy custom lua file
+cp "$HOME/dotfiles/.config/nvim/lua/kitty+page.lua" "$HOME/.config/nvim/lua" && \
+echo "Custom lua file copied successfully."
+
+# End of script
+echo "NVChad configuration installation complete."
