be1879989d016819133ecca6117d9a95b893abba
Twilight4 <electrolight071@gmail.com>
2024-06-30 18:51:49 +0200

    update

---
+++ .config/hypr/scripts/tm-open-all.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+~/.config/hypr/term-scripts/tm-webcam.sh
+~/.config/hypr/term-scripts/tm-musikcube.sh
+~/.config/hypr/term-scripts/tm-cava.sh
+~/.config/hypr/term-scripts/tm-clock.sh
+~/.config/hypr/term-scripts/tm-pipes.sh
+~/.config/hypr/term-scripts/tm-rain.sh
+~/.config/hypr/term-scripts/tm-cmatrix.sh
+~/.config/hypr/term-scripts/tm-neofetch.sh
+~/.config/hypr/term-scripts/tm-asciiquarium.sh
+~/.config/hypr/term-scripts/tm-fireplace.sh
