be11f0d83876daf8d35fd6d939404ab53609a930
Twilight4 <electrolight071@gmail.com>
2024-06-30 23:59:22 +0200

    update

--- .config/.install/wallpaper.sh
+++ .config/.install/wallpaper.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+clear
 cat <<"EOF"
                _ _                                 
 __      ____ _| | |_ __   __ _ _ __   ___ _ __ ___ 
@@ -10,35 +11,47 @@ __      ____ _| | |_ __   __ _ _ __   ___ _ __ ___
 
 EOF
 
-echo "Do you want to download the wallpapers from repository https://github.com/Twilight4/wallpapers.git ?"
-while true; do
-	read -p "(Yy/Nn): " yn
-	case $yn in
-	[Yy]*)
+install_full_repo() {
     if [ -d ~/pictures/wallpapers ]; then
         rm -rf ~/pictures/wallpapers 2>&1 | tee -a "$LOG"
     fi
-		mkdir -p ~/pictures/
-		git clone --depth 1 https://github.com/Twilight4/wallpapers ~/pictures/wallpapers
-		rm -rf ~/pictures/wallpapers/.git
-		echo "Wallpapers installed successfully."
+    mkdir -p ~/pictures/
+    git clone --depth 1 https://github.com/Twilight4/wallpapers ~/pictures/wallpapers
+    rm -rf ~/pictures/wallpapers/.git
+    echo "Wallpapers installed successfully."
     [ -f ~/pictures/wallpapers/default.jpg ] && cp ~/pictures/wallpapers/default.jpg ~/.cache/current_wallpaper.jpg && echo "Copied default.jpg" || echo "default.jpg does not exist."
     [ -f ~/pictures/wallpapers/lady.png ] && cp ~/pictures/wallpapers/lady.png ~/.cache/lady.png && echo "Copied lady.png" || echo "lady.png does not exist."
-		break
-		;;
-	[Nn]*)
-		mkdir -p ~/pictures/wallpapers
-		curl -LO https://raw.githubusercontent.com/Twilight4/wallpapers/main/default.jpg && mv default.jpg ~/pictures/wallpapers/default.jpg
-		curl -LO https://raw.githubusercontent.com/Twilight4/wallpapers/main/lady.png && mv lady.png ~/.cache/lady.png
-		echo "Default wallpaper installed successfully."
-		break
-		;;
-	*) echo "Please answer y/n." ;;
-	esac
-done
-echo ""
+}
 
-# GTK Themes for flatpak
-sudo flatpak override --filesystem=$HOME/.config/.local/share/themes
-sudo flatpak override --filesystem=$HOME/.config/.local/share/icons
-sudo flatpak override --filesystem=xdg-config/gtk-4.0
+install_default_wallpaper() {
+    mkdir -p ~/pictures/wallpapers
+    curl -LO https://raw.githubusercontent.com/Twilight4/wallpapers/main/default.jpg && mv default.jpg ~/pictures/wallpapers/default.jpg
+    curl -LO https://raw.githubusercontent.com/Twilight4/wallpapers/main/lady.png && mv lady.png ~/.cache/lady.png
+    echo "Default wallpaper installed successfully."
+}
+
+main() {
+    echo "Please choose an option:"
+    echo "1. Install full wallpapers repository"
+    echo "2. Install only the default wallpaper"
+    echo "3. Skip installation"
+    read -p "Enter your choice (1/2/3): " choice
+    case $choice in
+        1)
+            install_full_repo
+            ;;
+        2)
+            install_default_wallpaper
+            ;;
+        3)
+            echo "Installation skipped."
+            ;;
+        *)
+            echo "Invalid choice. Please enter 1, 2, or 3."
+            main
+            ;;
+    esac
+}
+
+main
+echo ""
