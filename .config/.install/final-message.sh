b1e9c9f7f8744964348da63f6d72839a97923e6d
Twilight4 <electrolight071@gmail.com>
2024-07-01 01:10:00 +0200

    update

--- .config/.install/final-message.sh
+++ .config/.install/final-message.sh
@@ -5,21 +5,26 @@ clear
 echo "-------------------------- INSTALLATION FINISHED -------------------------"
 echo "Please reboot your system."
 echo ""
-echo "Post-install reminder:"
-echo "1. Install appropriate kernel headers (if not installed):"
-echo "    apt search linux-headers-\$(uname -r)"
-echo "2. Import your OpenAI API key for emacs in '~/.authinfo':"
+echo "Generic post-install reminder:"
+echo " 1. Add myself to /etc/sudoers:"
+echo "    twilight ALL=(ALL:ALL) NOPASSWD: ALL"
+echo "    Defaults !tty_tickets"
+echo " 2. Check post-install: https://github.com/Twilight4/debian-setup"
+#echo "3. Install appropriate kernel headers (if not installed):"
+#echo "    apt search linux-headers-\$(uname -r)"
+echo " 3. Import your OpenAI API key for emacs in ~/.authinfo:"
 echo '    machine api.openai.com login apikey password <TOKEN_HERE>'
-echo '3. Copy AI prompts to emacs dir to source them:'
+echo ' 4. Copy AI prompts to emacs dir to source them:'
 echo '    cp ~/desktop/workspace/dotfiles/.config/ai-prompts ~/.cache/emacs'
-echo "4. Add pub key to github:"
+echo " 5. Add pub key to github:"
 echo '    ssh-keygen -t ed25519'
-echo "5. Clone relevant repos via SSH:"
+echo " 6. Clone relevant repos via SSH:"
 echo '    git clone git@github.com:Twilight4/dotfiles.git ~/desktop/workspace/dotfiles'
 echo '    git clone git@github.com:Twilight4/nobility.git ~/desktop/workspace/nobility'
 echo '    git clone git@github.com:Twilight4/debian-setup.git ~/desktop/workspace/debian-setup'
 echo '    git clone git@github.com:Twilight4/org.git ~/documents/org'
 echo '    git clone git@github.com:Twilight4/cheats.git ~/.config/cheat'
+echo " 7. Sync files with the cloud: backup.sh"
 echo ""
 
 # Prompt the user to reboot
