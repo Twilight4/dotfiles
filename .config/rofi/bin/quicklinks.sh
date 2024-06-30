059f0c5edf975188423ef3a565c0d5604a10ae04
Twilight4 <electrolight071@gmail.com>
2024-06-30 04:10:03 +0200

    update

--- .config/rofi/bin/quicklinks.sh
+++ .config/rofi/bin/quicklinks.sh
@@ -33,6 +33,7 @@ option_25="󰥷 Image to Text"
 option_26="󱇱 HackTricks Cloud"
 option_27=" Google Exploits"
 option_28="󰬎 GTFOBins"
+option_29=" RevShells"
 
 # Rofi CMD
 rofi_cmd() {
@@ -54,7 +55,7 @@ get_search_query() {
 7004dbcef0f854e0fb401875f26ebd00
 # Pass variables to rofi dmenu
 run_rofi() {
-  echo -e "$option_1\n$option_2\n$option_3\n$option_4\n$option_5\n$option_6\n$option_7\n$option_8\n$option_9\n$option_10\n$option_11\n$option_12\n$option_13\n$option_14\n$option_15\n$option_16\n$option_17\n$option_18\n$option_19\n$option_20\n$option_21\n$option_22\n$option_23\n$option_24\n$option_25\n$option_26\n$option_27\n$option_28" | rofi_cmd
+  echo -e "$option_1\n$option_2\n$option_3\n$option_4\n$option_5\n$option_6\n$option_7\n$option_8\n$option_9\n$option_10\n$option_11\n$option_12\n$option_13\n$option_14\n$option_15\n$option_16\n$option_17\n$option_18\n$option_19\n$option_20\n$option_21\n$option_22\n$option_23\n$option_24\n$option_25\n$option_26\n$option_27\n$option_28\n$option_29" | rofi_cmd
 }
 
 # Execute Command
@@ -195,6 +196,9 @@ run_cmd() {
     --opt28)
       xdg-open 'https://gtfobins.github.io/' &
       ;;
+    --opt29)
+      xdg-open 'https://www.revshells.com/' &
+      ;;
     *)
       ;;
   esac
@@ -286,5 +290,8 @@ case "${chosen}" in
     ;;
   $option_28)
     run_cmd --opt28
+    ;;
+  $option_29)
+    run_cmd --opt29
     ;;
 esac
