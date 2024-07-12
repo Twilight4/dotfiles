92030a31c89da35dc3f7e388b3c5c46e8f613c06
Twilight4 <electrolight071@gmail.com>
2024-07-12 14:46:57 +0200

    update

--- .config/rofi/bin/quicklinks.sh
+++ .config/rofi/bin/quicklinks.sh
@@ -36,6 +36,7 @@ option_28="󰬎 GTFOBins"
 option_29=" RevShells"
 option_30=" Crackstation"
 option_31=" Google Maps"
+option_32="󰰮 WADComs"
 
 # Rofi CMD
 rofi_cmd() {
@@ -56,7 +57,7 @@ get_search_query() {
 
 # Pass variables to rofi dmenu
 run_rofi() {
-  echo -e "$option_1\n$option_2\n$option_3\n$option_4\n$option_5\n$option_6\n$option_7\n$option_8\n$option_9\n$option_10\n$option_11\n$option_12\n$option_13\n$option_14\n$option_15\n$option_16\n$option_17\n$option_18\n$option_19\n$option_20\n$option_21\n$option_22\n$option_23\n$option_24\n$option_25\n$option_26\n$option_27\n$option_28\n$option_29\n$option_30\n$option_31" | rofi_cmd
+  echo -e "$option_1\n$option_2\n$option_3\n$option_4\n$option_5\n$option_6\n$option_7\n$option_8\n$option_9\n$option_10\n$option_11\n$option_12\n$option_13\n$option_14\n$option_15\n$option_16\n$option_17\n$option_18\n$option_19\n$option_20\n$option_21\n$option_22\n$option_23\n$option_24\n$option_25\n$option_26\n$option_27\n$option_28\n$option_29\n$option_30\n$option_31\n$option_32" | rofi_cmd
 }
 
 # Execute Command
@@ -306,5 +307,8 @@ case "${chosen}" in
     ;;
   $option_31)
     run_cmd --opt31
+    ;;
+  $option_32)
+    run_cmd --opt32
     ;;
 esac
