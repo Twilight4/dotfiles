a69a4d0c7946f5e4a397a18c26570e4fc9af3253
Twilight4 <electrolight071@gmail.com>
2024-07-23 19:15:07 +0200

    update

--- .config/rofi/bin/quicklinks.sh
+++ .config/rofi/bin/quicklinks.sh
@@ -37,6 +37,7 @@ option_29=" RevShells"
 option_30=" Crackstation"
 option_31=" Google Maps"
 option_32="󰰮 WADComs"
+option_33="󰩃 BloodHound CheatSheet"
 
 # Rofi CMD
 rofi_cmd() {
@@ -57,7 +58,7 @@ get_search_query() {
 
 # Pass variables to rofi dmenu
 run_rofi() {
-  echo -e "$option_1\n$option_2\n$option_3\n$option_4\n$option_5\n$option_6\n$option_7\n$option_8\n$option_9\n$option_10\n$option_11\n$option_12\n$option_13\n$option_14\n$option_15\n$option_16\n$option_17\n$option_18\n$option_19\n$option_20\n$option_21\n$option_22\n$option_23\n$option_24\n$option_25\n$option_26\n$option_27\n$option_28\n$option_29\n$option_30\n$option_31\n$option_32" | rofi_cmd
+  echo -e "$option_1\n$option_2\n$option_3\n$option_4\n$option_5\n$option_6\n$option_7\n$option_8\n$option_9\n$option_10\n$option_11\n$option_12\n$option_13\n$option_14\n$option_15\n$option_16\n$option_17\n$option_18\n$option_19\n$option_20\n$option_21\n$option_22\n$option_23\n$option_24\n$option_25\n$option_26\n$option_27\n$option_28\n$option_29\n$option_30\n$option_31\n$option_32\n$option_33" | rofi_cmd
 }
 
 # Execute Command
@@ -210,6 +211,9 @@ run_cmd() {
     --opt32)
       xdg-open 'https://www.google.com/maps' &
       ;;
+    --opt34)
+      xdg-open 'https://hausec.com/2019/09/09/bloodhound-cypher-cheatsheet/' &
+      ;;
     *)
       ;;
   esac
@@ -313,5 +317,8 @@ case "${chosen}" in
     ;;
   $option_32)
     run_cmd --opt32
+    ;;
+  $option_33)
+    run_cmd --opt33
     ;;
 esac
