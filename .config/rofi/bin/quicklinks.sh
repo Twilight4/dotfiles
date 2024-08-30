83c7cb2d568837b75b1321ec4ac7c387dc1d1809
Twilight4 <electrolight071@gmail.com>
2024-08-30 05:30:39 +0200

    Auto-commit: local changes

--- .config/rofi/bin/quicklinks.sh
+++ .config/rofi/bin/quicklinks.sh
@@ -43,6 +43,7 @@ option_35=" Digital Ocean"
 option_36=" PayPal"
 option_37=" SharpCollection"
 option_38=" 0bin"
+option_39=" Tutanoda Mail"
 
 # Rofi CMD
 rofi_cmd() {
@@ -63,7 +64,7 @@ get_search_query() {
 
 # Pass variables to rofi dmenu
 run_rofi() {
-  echo -e "$option_1\n$option_2\n$option_3\n$option_4\n$option_5\n$option_6\n$option_7\n$option_8\n$option_9\n$option_10\n$option_11\n$option_12\n$option_13\n$option_14\n$option_15\n$option_16\n$option_17\n$option_18\n$option_19\n$option_20\n$option_21\n$option_22\n$option_23\n$option_24\n$option_25\n$option_26\n$option_27\n$option_28\n$option_29\n$option_30\n$option_31\n$option_32\n$option_33\n$option_34\n$option_35\n$option_36\n$option_37\n$option_38" | rofi_cmd
+  echo -e "$option_1\n$option_2\n$option_3\n$option_4\n$option_5\n$option_6\n$option_7\n$option_8\n$option_9\n$option_10\n$option_11\n$option_12\n$option_13\n$option_14\n$option_15\n$option_16\n$option_17\n$option_18\n$option_19\n$option_20\n$option_21\n$option_22\n$option_23\n$option_24\n$option_25\n$option_26\n$option_27\n$option_28\n$option_29\n$option_30\n$option_31\n$option_32\n$option_33\n$option_34\n$option_35\n$option_36\n$option_37\n$option_38\n$option_39" | rofi_cmd
 }
 
 # Execute Command
@@ -234,6 +235,9 @@ run_cmd() {
     --opt38)
       xdg-open 'https://0bin.net/' &
       ;;
+    --opt39)
+      xdg-open 'https://app.tuta.com/mail/' &
+      ;;
     *)
       ;;
   esac
@@ -355,5 +359,8 @@ case "${chosen}" in
     ;;
   $option_38)
     run_cmd --opt38
+    ;;
+  $option_39)
+    run_cmd --opt39
     ;;
 esac
