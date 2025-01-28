7331813a323ebb83af3c46565fb056361f31c536
Twilight4 <99445042+Twilight4@users.noreply.github.com>
2025-01-28 09:11:48 +0100

    update

--- .config/rofi/bin/quicklinks.sh
+++ .config/rofi/bin/quicklinks.sh
@@ -54,6 +54,9 @@ option_46=" AD Github CheatSheet"
 option_47="󰰦 Temu"
 option_48="󰄐 AliExpress"
 option_49=" Vinted"
+option_50=" Polregio"
+option_51=" PKP Intercity"
+option_52="󰃧 Jakdojade"
 
 # Rofi CMD
 rofi_cmd() {
@@ -74,7 +77,7 @@ get_search_query() {
 
 # Pass variables to rofi dmenu
 run_rofi() {
-  echo -e "$option_1\n$option_2\n$option_3\n$option_4\n$option_5\n$option_6\n$option_7\n$option_8\n$option_9\n$option_10\n$option_11\n$option_12\n$option_13\n$option_14\n$option_15\n$option_16\n$option_17\n$option_18\n$option_19\n$option_20\n$option_21\n$option_22\n$option_23\n$option_24\n$option_25\n$option_26\n$option_27\n$option_28\n$option_29\n$option_30\n$option_31\n$option_32\n$option_33\n$option_34\n$option_35\n$option_36\n$option_37\n$option_38\n$option_39\n$option_40\n$option_41\n$option_42\n$option_43\n$option_44\n$option_45\n$option_46\n$option_47\n$option_48\n$option_49" | rofi_cmd
+  echo -e "$option_1\n$option_2\n$option_3\n$option_4\n$option_5\n$option_6\n$option_7\n$option_8\n$option_9\n$option_10\n$option_11\n$option_12\n$option_13\n$option_14\n$option_15\n$option_16\n$option_17\n$option_18\n$option_19\n$option_20\n$option_21\n$option_22\n$option_23\n$option_24\n$option_25\n$option_26\n$option_27\n$option_28\n$option_29\n$option_30\n$option_31\n$option_32\n$option_33\n$option_34\n$option_35\n$option_36\n$option_37\n$option_38\n$option_39\n$option_40\n$option_41\n$option_42\n$option_43\n$option_44\n$option_45\n$option_46\n$option_47\n$option_48\n$option_49\n$option_50\n$option_51\n$option_52" | rofi_cmd
 }
 
 # Execute Command
@@ -276,7 +279,16 @@ run_cmd() {
       xdg-open 'https://best.aliexpress.com/?gatewayAdapt=pol2usa&browser_redirect=true' &
       ;;
     --opt49)
-      xdg-open 'https://www.vinted.com/' &
+      xdg-open 'https://www.vinted.pl/' &
+      ;;
+    --opt50)
+      xdg-open 'https://bilety.polregio.pl/' &
+      ;;
+    --opt51)
+      xdg-open 'https://ebilet.intercity.pl/' &
+      ;;
+    --opt52)
+      xdg-open 'https://jakdojade.pl/' &
       ;;
     *)
       ;;
@@ -432,5 +444,14 @@ case "${chosen}" in
     ;;
   $option_49)
     run_cmd --opt49
+    ;;
+  $option_50)
+    run_cmd --opt50
+    ;;
+  $option_51)
+    run_cmd --opt51
+    ;;
+  $option_52)
+    run_cmd --opt52
     ;;
 esac
