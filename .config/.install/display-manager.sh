8d17af547809fe3e20cc55e048e0fe642d9ccadc
Twilight4 <99445042+Twilight4@users.noreply.github.com>
2025-03-10 19:01:00 +0100

    Improve display-manager.sh installation script

--- .config/.install/display-manager.sh
+++ .config/.install/display-manager.sh
@@ -11,34 +11,26 @@ ____  _           _               __  __
 
 EOF
 
-	while true; do
-		read -p "Do you want to install SDDM with theme (Yy/Nn): " yn
-		case $yn in
-		[Yy]*)
-			_installPackagesParu sddm-git
-			_installPackagesParu sddm-sugar-candy-git
+# Prompt the user
+read -p "This will configure SDDM display manager. Press any key to continue or Ctrl+C to exit..." -n 1 -s
+echo
 
-			echo "Creating /etc/sddm.conf file..."
-			sudo mv /etc/sddm.conf /etc/sddm.conf.bak
+echo "Creating /etc/sddm.conf file..."
+sudo mv /etc/sddm.conf /etc/sddm.conf.bak
 
-			# Clone the sddm.conf config file
-			curl -LJO https://raw.githubusercontent.com/Twilight4/dotfiles/main/.config/sddm/sddm.conf && sudo mv sddm.conf /etc/sddm.conf
+# Clone the sddm.conf config file
+curl -LJO https://raw.githubusercontent.com/Twilight4/dotfiles/main/.config/sddm/sddm.conf && sudo mv sddm.conf /etc/sddm.conf
 
-			# Apply the wallpaper background to sddm
-			curl -LJO https://raw.githubusercontent.com/Twilight4/dotfiles/refs/heads/main/.config/sddm/default.png && sudo mv default.png /usr/share/sddm/themes/sugar-candy/Backgrounds/default.png
+# Download the default.png image
+curl -LJO https://raw.githubusercontent.com/Twilight4/dotfiles/refs/heads/main/.config/sddm/melbourne.jpg && sudo mv melbourne.jpg /usr/share/sddm/themes/sugar-candy/Backgrounds/melbourne.jpg
 
-			# Clone the theme.conf config file
-			curl -LJO https://raw.githubusercontent.com/Twilight4/dotfiles/main/.config/sddm/theme.conf && sudo mv theme.conf /usr/share/sddm/themes/sugar-candy/theme.conf
+# Change the default background image to default.png image
+sudo sed -i 's|Background="Backgrounds/Mountain.jpg"|Background="Backgrounds/melbourne.jpg"|g' /usr/share/sddm/themes/sugar-candy/theme.conf
 
-			echo "/etc/sddm.conf file created."
-			break
-			;;
-		[Nn]*)
-			echo "SDDM setup skipped."
-			break
-			;;
-		*) echo "Please answer yes or no." ;;
-		esac
-	done
-	echo ""
-fi
+# Clone the theme.conf config file
+curl -LJO https://raw.githubusercontent.com/Twilight4/dotfiles/main/.config/sddm/theme.conf && sudo mv theme.conf /usr/share/sddm/themes/sugar-candy/theme.conf
+
+echo "/etc/sddm.conf file created."
+
+# Wait 2 sec before clear so user knows what happened
+sleep 2
