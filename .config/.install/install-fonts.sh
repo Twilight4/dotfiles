0d330491b9b17a18a2bb2e642b399dc5a0241cbf
Twilight4 <99445042+Twilight4@users.noreply.github.com>
2025-03-10 20:28:54 +0100

    Improve install-fonts.sh installation script

--- .config/.install/install-fonts.sh
+++ .config/.install/install-fonts.sh
@@ -10,72 +10,67 @@ cat <<"EOF"
 
 EOF
 
-# Confirmation prompt
-read -p "Do you want to install fonts? (y/n) " answer
-case ${answer:0:1} in
-    y|Y )
-        # Jetbrains nerd font
-        echo
-        echo "Installing fonts..."
-        echo "Downloading and Extracting Jetbrains Mono Nerd Font..."
-        DOWNLOAD_URL="https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.tar.xz"
-        # Maximum number of download attempts
-        MAX_ATTEMPTS=3
-        for ((ATTEMPT = 1; ATTEMPT <= MAX_ATTEMPTS; ATTEMPT++)); do
-            curl -OL "$DOWNLOAD_URL"&& break
-            echo "Download attempt $ATTEMPT failed. Retrying in 2 seconds..."
-            sleep 2
-        done
+# Prompt the user
+read -p "This will install fonts. Press any key to continue or Ctrl+C to exit..." -n 1 -s
+echo
 
-        # Check if the JetBrainsMono folder exists and delete it if it does
-        if [ -d ~/.config/.local/share/fonts/JetBrainsMonoNerd ]; then
-            rm -rvf ~/.config/.local/share/fonts/JetBrainsMono
-        fi
+# Jetbrains nerd font
+echo
+echo "Installing fonts..."
+echo "Downloading and Extracting Jetbrains Mono Nerd Font..."
+DOWNLOAD_URL="https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.tar.xz"
+# Maximum number of download attempts
+MAX_ATTEMPTS=3
+for ((ATTEMPT = 1; ATTEMPT <= MAX_ATTEMPTS; ATTEMPT++)); do
+    curl -OL "$DOWNLOAD_URL"&& break
+    echo "Download attempt $ATTEMPT failed. Retrying in 2 seconds..."
+    sleep 2
+done
 
-        mkdir -pv ~/.config/.local/share/fonts/JetBrainsMono
+# Check if the JetBrainsMono folder exists and delete it if it does
+if [ -d ~/.config/.local/share/fonts/JetBrainsMonoNerd ]; then
+    rm -rvf ~/.config/.local/share/fonts/JetBrainsMono
+fi
 
-        # Extract the new files into the JetBrainsMono folder and log the output
-        tar -xJkf JetBrainsMono.tar.xz -C ~/.config/.local/share/fonts/JetBrainsMono
+mkdir -pv ~/.config/.local/share/fonts/JetBrainsMono
 
-        # Cleanup
-        rm -rf JetBrainsMono.tar.xz
+# Extract the new files into the JetBrainsMono folder and log the output
+tar -xJkf JetBrainsMono.tar.xz -C ~/.config/.local/share/fonts/JetBrainsMono
+
+# Cleanup
+rm -rf JetBrainsMono.tar.xz
 
 
-        # Meslo nerd font
-        echo
-        echo "Downloading and Extracting Meslo Nerd Font..."
-        DOWNLOAD_URL="https://github.com/ryanoasis/nerd-fonts/releases/latest/download/Meslo.tar.xz"
-        # Maximum number of download attempts
-        MAX_ATTEMPTS=3
-        for ((ATTEMPT = 1; ATTEMPT <= MAX_ATTEMPTS; ATTEMPT++)); do
-            curl -OL "$DOWNLOAD_URL" && break
-            echo "Download attempt $ATTEMPT failed. Retrying in 2 seconds..."
-            sleep 2
-        done
+# Meslo nerd font
+echo
+echo "Downloading and Extracting Meslo Nerd Font..."
+DOWNLOAD_URL="https://github.com/ryanoasis/nerd-fonts/releases/latest/download/Meslo.tar.xz"
+# Maximum number of download attempts
+MAX_ATTEMPTS=3
+for ((ATTEMPT = 1; ATTEMPT <= MAX_ATTEMPTS; ATTEMPT++)); do
+    curl -OL "$DOWNLOAD_URL" && break
+    echo "Download attempt $ATTEMPT failed. Retrying in 2 seconds..."
+    sleep 2
+done
 
-        # Check if the Meslo folder exists and delete it if it does
-        if [ -d ~/.config/.local/share/fonts/Meslo ]; then
-            rm -rfv ~/.config/.local/share/fonts/Meslo
-        fi
+# Check if the Meslo folder exists and delete it if it does
+if [ -d ~/.config/.local/share/fonts/Meslo ]; then
+    rm -rfv ~/.config/.local/share/fonts/Meslo
+fi
 
-        mkdir -pv ~/.config/.local/share/fonts/Meslo
+mkdir -pv ~/.config/.local/share/fonts/Meslo
 
-        # Extract the new files into the Meslo folder and log the output
-        tar -xJkf Meslo.tar.xz -C ~/.config/.local/share/fonts/Meslo
+# Extract the new files into the Meslo folder and log the output
+tar -xJkf Meslo.tar.xz -C ~/.config/.local/share/fonts/Meslo
 
-        # Cleanup
-        rm -rf Meslo.tar.xz
+# Cleanup
+rm -rf Meslo.tar.xz
 
-        # Update font cache and log the output
-        fc-cache -v
+# Update font cache and log the output
+fc-cache -v
 
-        echo
-        echo "Fonts installed successfully."
-        ;;
-    * )
-        echo "Skipping fonts installation..."
-        ;;
-esac
+echo
+echo "Fonts installed successfully."
 
 # Wait 2 sec before clear so user knows what happened
 sleep 2
