76879c7e2769145e5bb802bc297fe15de5681840
Twilight4 <99445042+Twilight4@users.noreply.github.com>
2025-03-11 19:46:59 +0100

    Improve remove-buttons.sh installation script

--- .config/.install/remove-buttons.sh
+++ .config/.install/remove-buttons.sh
@@ -10,30 +10,30 @@ cat <<"EOF"
 
 EOF
                                     
-read -p "Do you want to remove the GTK button layout? (y/n): " update_layout_choice
+# Prompt the user
+read -p "This will remove the GTK button layout. Press any key to continue or Ctrl+C to exit..." -n 1 -s
+echo
 
-if [[ "$update_layout_choice" =~ ^[Yy]$ ]]; then
-    # Define the desired button layout value (remove buttons - none)
-    desired_button_layout=":"
+# Define the desired button layout value (remove buttons - none)
+desired_button_layout=":"
 
-    # Get the current button layout value using gsettings
-    current_button_layout=$(gsettings get org.gnome.desktop.wm.preferences button-layout)
+# Get the current button layout value using gsettings
+current_button_layout=$(gsettings get org.gnome.desktop.wm.preferences button-layout)
 
-    # Compare the current value with the desired value using an if statement
-    if [ "$current_button_layout" != "$desired_button_layout" ]; then
-        # If they don't match, update the button layout using the gsettings command
-        gsettings set org.gnome.desktop.wm.preferences button-layout "$desired_button_layout"
-        echo
-        echo "Button layout has been updated."
-    else
-        # If they match, display a message indicating that the value is already as desired
-        echo
-        echo "Button layout is already set as desired."
-    fi
-else
+# Compare the current value with the desired value using an if statement
+if [ "$current_button_layout" != "$desired_button_layout" ]; then
+    # If they don't match, update the button layout using the gsettings command
+    gsettings set org.gnome.desktop.wm.preferences button-layout "$desired_button_layout"
     echo
-    echo "Button layout update canceled by user."
+    echo "Button layout has been updated."
+else
+    # If they match, display a message indicating that the value is already as desired
+    echo
+    echo "Button layout is already set as desired."
 fi
+
+echo
+echo "Button layout update canceled by user."
 
 # Wait 2 sec before clear so user knows what happened
 sleep 2
