083d416142daae4630ca0641aa6eeed05bc73ffa
Twilight4 <99445042+Twilight4@users.noreply.github.com>
2025-03-27 20:21:39 +0100

    Add link to quicklinks.sh

--- .config/rofi/bin/quicklinks.sh
+++ .config/rofi/bin/quicklinks.sh
@@ -52,6 +52,7 @@ option_62=" Flaticon Icons"
 option_63="󱃾 Kubectl Cheat Sheet"
 option_64="󱃾 Kubernetes Nginx Ingress Docs"
 option_65=" Tinder"
+option_66="󰃧 PEKA card"
 
 # Rofi CMD
 rofi_cmd() {
@@ -72,7 +73,7 @@ get_search_query() {
 
 # Pass variables to rofi dmenu
 run_rofi() {
-  echo -e "$option_1\n$option_2\n$option_3\n$option_4\n$option_5\n$option_6\n$option_7\n$option_8\n$option_9\n$option_10\n$option_11\n$option_12\n$option_13\n$option_22\n$option_23\n$option_25\n$option_31\n$option_34\n$option_36\n$option_38\n$option_39\n$option_40\n$option_41\n$option_42\n$option_43\n$option_44\n$option_45\n$option_46\n$option_47\n$option_48\n$option_49\n$option_50\n$option_51\n$option_52\n$option_53\n$option_54\n$option_55\n$option_56\n$option_57\n$option_58\n$option_59\n$option_60\n$option_61\n$option_62\n$option_63\n$option_64\n$option_65" | rofi_cmd
+  echo -e "$option_1\n$option_2\n$option_3\n$option_4\n$option_5\n$option_6\n$option_7\n$option_8\n$option_9\n$option_10\n$option_11\n$option_12\n$option_13\n$option_22\n$option_23\n$option_25\n$option_31\n$option_34\n$option_36\n$option_38\n$option_39\n$option_40\n$option_41\n$option_42\n$option_43\n$option_44\n$option_45\n$option_46\n$option_47\n$option_48\n$option_49\n$option_50\n$option_51\n$option_52\n$option_53\n$option_54\n$option_55\n$option_56\n$option_57\n$option_58\n$option_59\n$option_60\n$option_61\n$option_62\n$option_63\n$option_64\n$option_65\n$option_66" | rofi_cmd
 }
 
 # Execute Command
@@ -268,6 +269,9 @@ run_cmd() {
     --opt65)
       xdg-open 'https://tinder.com/app/recs' &
       ;;
+    --opt66)
+      xdg-open 'https://www.peka.poznan.pl/km/tpursetopup' &
+      ;;
     *)
       ;;
   esac
@@ -416,5 +420,8 @@ case "${chosen}" in
     ;;
   $option_65)
     run_cmd --opt65
+    ;;
+  $option_66)
+    run_cmd --opt66
     ;;
 esac
