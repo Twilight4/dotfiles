c7b5fcfd35897f06e19e49a240dee8086123509e
Twilight4 <99445042+Twilight4@users.noreply.github.com>
2025-11-09 20:47:26 +0100

    update

--- .config/rofi/bin/quicklinks.sh
+++ .config/rofi/bin/quicklinks.sh
@@ -54,6 +54,7 @@ option_64="󱃾 Kubernetes Nginx Ingress Docs"
 option_65=" Tinder"
 option_66="󰃧 PEKA card"
 option_67=" Github Actions Events"
+option_68=" Github Marketplace"
 
 # Rofi CMD
 rofi_cmd() {
@@ -74,7 +75,7 @@ get_search_query() {
 
 # Pass variables to rofi dmenu
 run_rofi() {
-  echo -e "$option_1\n$option_2\n$option_3\n$option_4\n$option_5\n$option_6\n$option_7\n$option_8\n$option_9\n$option_10\n$option_11\n$option_12\n$option_13\n$option_22\n$option_23\n$option_25\n$option_31\n$option_34\n$option_36\n$option_38\n$option_39\n$option_40\n$option_41\n$option_42\n$option_43\n$option_44\n$option_45\n$option_46\n$option_47\n$option_48\n$option_49\n$option_50\n$option_51\n$option_52\n$option_53\n$option_54\n$option_55\n$option_56\n$option_57\n$option_58\n$option_59\n$option_60\n$option_61\n$option_62\n$option_63\n$option_64\n$option_65\n$option_66\n$option_67" | rofi_cmd
+  echo -e "$option_1\n$option_2\n$option_3\n$option_4\n$option_5\n$option_6\n$option_7\n$option_8\n$option_9\n$option_10\n$option_11\n$option_12\n$option_13\n$option_22\n$option_23\n$option_25\n$option_31\n$option_34\n$option_36\n$option_38\n$option_39\n$option_40\n$option_41\n$option_42\n$option_43\n$option_44\n$option_45\n$option_46\n$option_47\n$option_48\n$option_49\n$option_50\n$option_51\n$option_52\n$option_53\n$option_54\n$option_55\n$option_56\n$option_57\n$option_58\n$option_59\n$option_60\n$option_61\n$option_62\n$option_63\n$option_64\n$option_65\n$option_66\n$option_67\n$option_68" | rofi_cmd
 }
 
 # Execute Command
@@ -276,6 +277,9 @@ run_cmd() {
     --opt67)
       xdg-open 'https://docs.github.com/en/actions/reference/workflows-and-actions/events-that-trigger-workflows' &
       ;;
+    --opt68)
+      xdg-open 'https://github.com/marketplace' &
+      ;;
     *)
       ;;
   esac
@@ -430,5 +434,8 @@ case "${chosen}" in
     ;;
   $option_67)
     run_cmd --opt67
+    ;;
+  $option_68)
+    run_cmd --opt68
     ;;
 esac
