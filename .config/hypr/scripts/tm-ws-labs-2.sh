7fbf0ff879843efc666c9767abc720ef45aac8f7
Twilight4 <99445042+Twilight4@users.noreply.github.com>
2026-01-05 22:20:40 +0100

    update hypr configs

--- .config/hypr/scripts/tm-ws-labs-2.sh
+++ .config/hypr/scripts/tm-ws-labs-2.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 hyprctl dispatch exec 'kitty -T cpu-temp --class cpu-temp -e watch -n 0.5 sensors zenpower-pci-00c3'
-hyprctl dispatch exec 'kitty -T fans --class fans -e watch -n 0.5 sudo ~/.config/.local/bin/omen-fan i'
 hyprctl dispatch exec 'kitty -T htop-btop-ptop --class htop-btop-ptop --session ~/.config/kitty/session-float'
 hyprctl dispatch exec 'kitty -T nvtop --class nvtop -e nvtop'
 hyprctl dispatch exec 'kitty -T wavemon --class wavemon -e wavemon'
