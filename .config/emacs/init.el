cf4e9206be0ae8e7c484d5f28627441f535e143c
Twilight4 <99445042+Twilight4@users.noreply.github.com>
2026-06-04 13:00:57 +0200

    add emacs init.el file

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -1,3 +1,5 @@
-;; Twilight4's minimal emacs config 
+;; Elpaca package manager and use-package bootstrap
+(load (expand-file-name "~/.config/emacs/emacs-common-init.el"))
 
+;; Twilight4's minimal emacs config
 (org-babel-load-file (expand-file-name "~/.config/emacs/config.org"))
