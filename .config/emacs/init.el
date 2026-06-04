f4f83b1e3e781b544b8d0475b010c76fd92caa27
Twilight4 <99445042+Twilight4@users.noreply.github.com>
2026-06-04 15:34:02 +0200

    Auto-commit: local changes

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -1,7 +1,4 @@
 ;;;  -*- lexical-binding: t; -*-
 
-;; Elpaca package manager and use-package bootstrap
-(load (expand-file-name "~/.config/emacs/emacs-common-init.el"))
-
 ;; Twilight4's minimal emacs config
 (org-babel-load-file (expand-file-name "~/.config/emacs/config.org"))
