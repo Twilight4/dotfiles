1c8c99c71e183f051d1e14debacf08759e70418d
Twilight4 <99445042+Twilight4@users.noreply.github.com>
2026-06-12 23:13:08 +0200

    remove redundant completions

--- .config/zsh/completion.zsh
+++ .config/zsh/completion.zsh
@@ -1,99 +1,10 @@
 ###########
 # General #
 ###########
-# Load more completions
-fpath=($XDG_CONFIG_HOME/zsh/plugins/zsh-completions/src $fpath)
-
-local cheats taglist pathlist
-
-_cheat_complete_personal_cheatsheets()
-{
-  cheats=("${(f)$(cheat -l -t personal | tail -n +2 | cut -d' ' -f1)}")
-  _describe -t cheats 'cheats' cheats
-}
-
-_cheat_complete_full_cheatsheets()
-{
-  cheats=("${(f)$(cheat -l | tail -n +2 | cut -d' ' -f1)}")
-  _describe -t cheats 'cheats' cheats
-}
-
-_cheat_complete_tags()
-{
-  taglist=("${(f)$(cheat -T)}")
-  _describe -t taglist 'taglist' taglist
-}
-
-_cheat_complete_paths()
-{
-  pathlist=("${(f)$(cheat -d | cut -d':' -f1)}")
-  _describe -t pathlist 'pathlist' pathlist
-}
-
-_cheat() {
-
-  _arguments -C \
-    '(--init)--init[Write a default config file to stdout]: :->none' \
-    '(-c --colorize)'{-c,--colorize}'[Colorize output]: :->none' \
-    '(-d --directories)'{-d,--directories}'[List cheatsheet directories]: :->none' \
-    '(-e --edit)'{-e,--edit}'[Edit <sheet>]: :->personal' \
-    '(-l --list)'{-l,--list}'[List cheatsheets]: :->full' \
-    '(-p --path)'{-p,--path}'[Return only sheets found on path <name>]: :->pathlist' \
-    '(-r --regex)'{-r,--regex}'[Treat search <phrase> as a regex]: :->none' \
-    '(-s --search)'{-s,--search}'[Search cheatsheets for <phrase>]: :->none' \
-    '(-t --tag)'{-t,--tag}'[Return only sheets matching <tag>]: :->taglist' \
-    '(-T --tags)'{-T,--tags}'[List all tags in use]: :->none' \
-    '(-v --version)'{-v,--version}'[Print the version number]: :->none' \
-    '(--rm)--rm[Remove (delete) <sheet>]: :->personal' 
-
-  case $state in
-    (none)
-      ;;
-    (full)
-      _cheat_complete_full_cheatsheets
-      ;;
-    (personal)
-      _cheat_complete_personal_cheatsheets
-      ;;
-    (taglist)
-      _cheat_complete_tags
-      ;;
-    (pathlist)
-      _cheat_complete_paths
-      ;;
-    (*)
-      ;;
-  esac
-}
-
-# Should be called before compinit
-zmodload zsh/complist
-
-# Use hjlk in menu selection (during completion)
-# Doesn't work well with interactive mode
-bindkey -M menuselect 'h' vi-backward-char
-bindkey -M menuselect 'k' vi-up-line-or-history
-bindkey -M menuselect 'j' vi-down-line-or-history
-bindkey -M menuselect 'l' vi-forward-char
-
-bindkey -M menuselect '^xg' clear-screen
-bindkey -M menuselect '^xi' vi-insert                      # Insert
-bindkey -M menuselect '^xh' accept-and-hold                # Hold
-bindkey -M menuselect '^xn' accept-and-infer-next-history  # Next
-bindkey -M menuselect '^xu' undo                           # Undo
-
-autoload -U compinit; compinit
 _comp_options+=(globdots) # With hidden files
 
 # automatically load bash completion functions
 autoload -U +X bashcompinit && bashcompinit
-
-# Only work with the Zsh function vman
-# See $DOTFILES/zsh/scripts.zsh
-compdef vman="man"
-
-# load cheat completions
-compdef _cheat cheat
 
 # load wrapper script for hyperlinked grep
 compdef _rg hg
