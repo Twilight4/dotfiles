cfc77d4e60014aa96daa37835fddbf6fed052213
Twilight4 <99445042+Twilight4@users.noreply.github.com>
2026-06-13 05:03:04 +0200

    optimizing hyprland workflow

--- .config/hypr/scripts/tm-ws-labs-2.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-hyprctl dispatch exec 'kitty -T cpu-temp --class cpu-temp -e watch -n 0.5 sensors zenpower-pci-00c3'
-hyprctl dispatch exec 'kitty -T htop-btop-ptop --class htop-btop-ptop --session ~/.config/kitty/session-float'
-hyprctl dispatch exec 'kitty -T nvtop --class nvtop -e nvtop'
-hyprctl dispatch exec 'kitty -T wavemon --class wavemon -e wavemon'
