d3bc6335189c46a17965c112bfd486f1f317ae3b
Twilight4 <99445042+Twilight4@users.noreply.github.com>
2026-06-28 19:47:40 +0200

    update webcam script

--- .config/.local/bin/webcam.sh
+++ .config/.local/bin/webcam.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
-ffplay -window_title Webcam -fast /dev/video0
+ffplay -window_title Webcam \
+  -f v4l2 \
+  -input_format mjpeg \
+  -video_size 1920x1080 \
+  -framerate 30 \
+  -fflags nobuffer \
+  -flags low_delay \
+  -framedrop \
+  /dev/video0
